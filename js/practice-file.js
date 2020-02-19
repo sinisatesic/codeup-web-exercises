c80aa54e0105e648b0aef90e993655f8002bd888
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-02-19 16:24:03 -0600

    added more onto practice file js

--- js/practice-file.js
+++ js/practice-file.js
@@ -8,6 +8,7 @@ function fruit(name,color,shape){
     }
 }
 
+
 let melon = new fruit('melon','green','round');
 console.log(melon.describe());
 
@@ -335,3 +336,38 @@ function matchHouses(step) {
 }
 ////
 ////
+///edabite shite: use as third pod problem (hard difficulty):
+//Create a Book constructor that has two properties :
+//
+// Title
+// Author
+// and two methods:
+//
+// A method named getTitle that returns: "Title: " + the instance title.
+// A method named getAuthor that returns: "Author: " + the instance author.
+// and instantiate this constructor by creating 3 new books:
+//
+// Pride and Prejudice - Jane Austen (PP)
+// Hamlet - William Shakespeare (H)
+// War and Peace - Leo Tolstoy (WP)
+// Name the new object instances PP, H, and WP, respectively.
+//
+// For instance, if I instantiated the following book using this Book constructor function:
+//
+// Harry Potter - J.K. Rowling (HP)
+
+
+function Book(title,author){
+    this.title = title;
+    this.author = author;
+
+    this.getTitle = function(){
+        return "Title: " + this.title;
+    }
+    this.getAuthor = function(){
+        return "Author: " + this.author;
+    }
+}
+let PP = new Book('Pride and Prejudice','Jane Austen');
+let H = new Book('Hamlet','William Shakespeare');
+let WP = new Book('War and Peace', 'Leo Tolstoy');
