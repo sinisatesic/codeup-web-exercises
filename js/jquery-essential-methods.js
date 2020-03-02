d9016954f44c15a49178815e77070d874106e71f
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-03-02 09:30:27 -0600

    added jquery essential methods exercises

---
+++ js/jquery-essential-methods.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// .html()
+//   .css()
+//     .addClass()
+//       .removeClass()
+//         .toggleClass()
+
+
+// ******* Getters and Setters **********
+
+// Getter for text of logo color
+
+// Setter for text of logo color
+
+// Getter for section color
+
+// Setter for the section color
+    // change all sections if 'all' is passed in
+    // if individual section is passed in, change its color (used for Rainbow effect)
+
+
+// reset all path elements to default, and change the 'color' text in the h2 to 'green'
+
+
+// write a function called rainbowLogo() that changes the logo to rainbow colors
+
+
+// Set a variable called circleClickCount, to keep track of how many times an individual circle has been clicked
+
+
+// Add a click listener to each of the 'circle' elements at the botto of the page, and make each color circle
+    // change the logo to the clicked color, or rainbow, if rainbow was clicked
+        // If the same color is clicked twice, toggle the 'caps' class to UPPERCASE and lowercase the color name
+
+
+// *********************** Traversing Methods ********************************* //
+
+// now let's traverse some elements
+
+// .each()   .first()    .last()    .children()    .parent()    .next()
+
+// TODO: Let's give each of the selection circles a 1px black border
+
+
+// TODO: Let's take the border away from the first circle
+
+
+// TODO: Let's make the last circle disappear
+
+
+// TODO: Find the children of the circle row, and remove borders from all of them, then unhide the last circle
+
+
+// TODO: while changing the fist path element to have a black fill, give its grandparent a light gray background and 10px padding
+
+
+// TODO: change the second path element to blue, then change the 'next' element to yellow
+
+
+// TODO: What's the difference between changing the class to change the color, and simply changing the color directly?
