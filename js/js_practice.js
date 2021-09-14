5c24f59ee89853e913a7c05fd454ca4f0a010cba
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-09-14 11:07:42 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -92,3 +92,29 @@ function derivative(b, m) {
 function possibleBonus(a, b) {
 	return (b-a) <= 6 && (b-a) > 0 ;
 }
+
+//
+
+// Folding a Piece of Paper
+// Create a function that returns the thickness (in meters) of a piece of paper after folding it n number of times. The paper starts off with a thickness of 0.5mm.
+
+// Examples
+// numLayers(1) ➞ "0.001m"
+// // Paper folded once is 1mm (equal to 0.001m)
+
+// numLayers(4) ➞ "0.008m"
+// // Paper folded 4 times is 8mm (equal to 0.008m)
+
+// numLayers(21) ➞ "1048.576m"
+// // Paper folded 21 times is 1048576mm (equal to 1048.576m)
+// Notes
+// There are 1000mm in a single meter.
+// Don't round answers.
+
+function numLayers(n) {
+	let ppr = 0.5;
+	for(let i = 0; i <= n; i++){
+		ppr+=ppr;
+	}
+	return `${(ppr/1000)/2}m`;
+}
