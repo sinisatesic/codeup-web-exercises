b6119239225d3a9c5b867c6ab8df2486bb13fbe1
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-09-14 10:55:09 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -74,4 +74,21 @@ function derivative(b, m) {
 
 //
 
+// In a board game, a piece may advance 1-6 tiles forward depending on the number rolled on a six-sided dice. If you advance your piece onto the same tile as another player's piece, both of you earn a bonus.
 
+// Given you and your friend's tile number, create a function that returns if it's possible to earn a bonus when you roll the dice.
+
+// Examples
+// possibleBonus(3, 7) ➞ true
+
+// possibleBonus(1, 9) ➞ false
+
+// possibleBonus(5, 3) ➞ false
+// Notes
+// You cannot move backward (which is why example #3 doesn't work).
+// If you are already on the same tile, return false, as you would be advancing away.
+// Expect only positive integer inputs.
+
+function possibleBonus(a, b) {
+	return (b-a) <= 6 && (b-a) > 0 ;
+}
