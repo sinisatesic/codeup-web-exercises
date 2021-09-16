a7fa096438d86ac4f0bf44f91af71ca2ebd2c199
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-09-16 11:08:53 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -257,3 +257,27 @@ let flattened = [[0, 1], [2, 3], [4, 5]].reduce(
 // flattened is [0, 1, 2, 3, 4, 5]
 
 //
+
+// Integer Digits Count
+// Create a function that counts the integer's number of digits.
+
+// Examples
+// count(318) ➞ 3
+ 
+// count(-92563) ➞ 5
+ 
+// count(4666) ➞ 4
+
+// count(-314890) ➞ 6
+
+// count(654321) ➞ 6
+
+// count(638476) ➞ 6
+// Notes
+// For an added challenge, try to solve this without using strings.
+// Alternatively, you can solve this via a recursive approach.
+// A reversion of this challenge can be found via this link.
+
+function count(n) {
+	return Math.abs(n).toString().length;
+}
