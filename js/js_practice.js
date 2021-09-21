219711a02b8442d68cc481c14b696b47ffd6f739
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-09-21 16:48:24 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -280,4 +280,59 @@ let flattened = [[0, 1], [2, 3], [4, 5]].reduce(
 
 function count(n) {
 	return Math.abs(n).toString().length;
+}// How Much is True?
+// Create a function which returns the number of true values there are in an array.
+
+// Examples
+// countTrue([true, false, false, true, false]) ➞ 2
+
+// countTrue([false, false, false, false]) ➞ 0
+
+// countTrue([]) ➞ 0
+// Notes
+// Return 0 if given an empty array.
+// All array items are of the type bool (true or false).
+
+function countTrue(arr) {
+    let counter = 0;
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] === true){
+            counter++;
+        } else if (arr === []){
+            counter = 0;
+        }
+    }
+   return counter;
 }
+
+// 
+
+const countTrue = r => r.filter(Boolean).length
+
+//
+
+// A Redundant Function
+// Write a function redundant that takes in a string str and returns a function that returns str.
+
+// Examples
+// const f1 = redundant("apple")
+// f1() ➞ "apple"
+
+// const f2 = redundant("pear")
+// f2() ➞ "pear"
+
+// const f3 = redundant("")
+// f3() ➞ ""
+// Notes
+// Your function should return a function, not a string.
+
+function redundant(str) {
+	return function(){
+		return str;
+	}
+}
+
+// 
+
+const redundant = str => () => str;
+
