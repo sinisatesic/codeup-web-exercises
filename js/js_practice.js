fe61fe1b4944b0dd833c6b0f942153e349c3a684
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-10-04 16:43:24 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -413,3 +413,49 @@ function getOlder(dogs) {
 
     return dogs;
 }
+
+
+// Calculate the Missing Value with Ohm's Law
+// Create a function that calculates the missing value of 3 inputs using Ohm's law. The inputs are v, r or i (aka: voltage, resistance and current).
+
+// Ohm's law:
+
+// V = R * I
+// Return the missing value rounded to two decimal places.
+
+// Examples
+// ohmsLaw(12, 220, "") ➞ 0.05
+
+// ohmsLaw(230, "", 2) ➞ 115
+
+// ohmsLaw("", 220, 0.02) ➞ 4.4
+
+// ohmsLaw("", "", 10) ➞ "Invalid"
+
+// ohmsLaw(500, 50, 10) ➞ "Invalid"
+// Notes
+// Missing values will be ""
+// If there is more than one missing value, or no missing value, return "Invalid"
+// Only numbers will be given.
+
+function ohmsLaw(v, r, i) {
+	if (typeof v === 'string' && typeof r === 'string' || 
+			typeof r === 'string' && typeof i === 'string' ||
+			typeof v === 'string' && typeof i === 'string'){
+		return "Invalid";
+	}
+	if (typeof v === 'string') { 
+		return Number((r * i).toFixed(2));
+	} 
+	if (typeof r === 'string') {
+		return Number((v/i).toFixed(2));
+	}  
+	if (typeof i === 'string') {
+		return Number((v/r).toFixed(2));
+	}
+	if(typeof v === 'number' 
+		 && typeof r === 'number' 
+		 && typeof i === 'number'){
+		return "Invalid";
+	}
+}
