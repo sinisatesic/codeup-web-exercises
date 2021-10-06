63442909e0bb7fff17409821bc891d32e6d96661
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-10-06 13:40:13 -0500

    brief js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -459,3 +459,42 @@ function ohmsLaw(v, r, i) {
 		return "Invalid";
 	}
 }
+
+// Add up the Numbers from a Single Number
+// Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+// Examples
+// addUp(4) ➞ 10
+
+// addUp(13) ➞ 91
+
+// addUp(600) ➞ 180300
+// Notes
+// Expect any positive number between 1 and 1000.
+
+function addUp(num) {
+	return (num * (num + 1))/2;
+  }
+
+//   Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+// Examples
+// minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+
+// minMax([2334454, 5]) ➞ [5, 2334454]
+
+// minMax([1]) ➞ [1, 1]
+// Notes
+// All test arrays will have at least one element and are valid.
+
+function minMax(arr) {
+	let min = arr[0];
+	let max = arr[0];
+		
+		for (var i = 1; i <= arr.length; i++){
+			let value = arr[i];
+			min = (value < min) ? value : min;
+			max = (value > max) ? value : max;
+		}
+		return [min, max];
+	}
