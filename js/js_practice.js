331d06cf9a8e162df40d7347d50009a775c1479a
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-10-07 13:04:36 -0500

    quick js prob

--- js/js_practice.js
+++ js/js_practice.js
@@ -498,3 +498,26 @@ function minMax(arr) {
 		}
 		return [min, max];
 	}
+
+// 	Seven Boom!
+// Create a function that takes an array of numbers and return "Boom!" if the digit 7 appears in the array. Otherwise, return "there is no 7 in the array".
+
+// Examples
+// sevenBoom([1, 2, 3, 4, 5, 6, 7]) ➞ "Boom!"
+// // 7 contains the number seven.
+
+// sevenBoom([8, 6, 33, 100]) ➞ "there is no 7 in the array"
+// // None of the items contain 7 within them.
+
+// sevenBoom([2, 55, 60, 97, 86]) ➞ "Boom!"
+// // 97 contains the number seven.
+// Notes
+// N/A
+
+function sevenBoom(arr){
+	if (arr.join().includes(7)){
+		return "Boom!";
+	} else {
+		return "there is no 7 in the array";
+	}
+}
