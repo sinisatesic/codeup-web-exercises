7da7c535a552371eb6abf557d1b89a9f50d61c73
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-10-08 13:25:09 -0500

    js probs

--- js/js_practice.js
+++ js/js_practice.js
@@ -521,3 +521,62 @@ function sevenBoom(arr){
 		return "there is no 7 in the array";
 	}
 }
+
+// Tower of Hanoi
+// There are three towers. The objective of the game is to move all the disks over to tower #3, but you can't place a larger disk onto a smaller disk. To play the game or learn more about the Tower of Hanoi, check the Resources tab.
+
+// Tower of Hanoi
+
+// Create a function that takes a number discs as an argument and returns the minimum amount of steps needed to complete the game.
+
+// Examples
+// towerHanoi(3) ➞ 7
+
+// towerHanoi(5) ➞ 31
+
+// towerHanoi(0) ➞ 0
+// Notes
+// The amount of discs is always a positive integer.
+// 1 disc can be changed per move.
+
+function towerHanoi(discs) {
+	return  Math.pow(2,discs)-1
+}
+
+towerHanoi=d=>2**d-1
+
+
+// Number of Boomerangs
+// A boomerang is a V-shaped sequence that is either upright or upside down. Specifically, a boomerang can be defined as: sub-array of length 3, with the first and last digits being the same and the middle digit being different.
+
+// Some boomerang examples:
+
+// [3, 7, 3], [1, -1, 1], [5, 6, 5]
+// Create a function that returns the total number of boomerangs in an array.
+
+// To illustrate:
+
+// [3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2]
+// // 3 boomerangs in this sequence:  [3, 7, 3], [1, 5, 1], [2, -2, 2]
+// Be aware that boomerangs can overlap, like so:
+
+// [1, 7, 1, 7, 1, 7, 1]
+// // 5 boomerangs (from left to right): [1, 7, 1], [7, 1, 7], [1, 7, 1], [7, 1, 7], and [1, 7, 1]
+// Examples
+// countBoomerangs([9, 5, 9, 5, 1, 1, 1]) ➞ 2
+
+// countBoomerangs([5, 6, 6, 7, 6, 3, 9]) ➞ 1
+
+// countBoomerangs([4, 4, 4, 9, 9, 9, 9]) ➞ 0
+// Notes
+// [5, 5, 5] (triple identical digits) is NOT considered a boomerang because the middle digit is identical to the first and last.
+
+function countBoomerangs(arr) {
+	let counterz = 0;
+	for(let i = 0; i < arr.length; i++){
+		if (arr[i] === arr[i + 2] && arr[i] !== arr[i + 1]){
+			counterz++;
+		}
+	}
+	return counterz;
+}
