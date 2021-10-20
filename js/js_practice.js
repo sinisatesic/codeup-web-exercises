d829bb85b390871805fe4bbd62ed87562e336b12
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-10-20 09:30:07 -0500

    js prob

--- js/js_practice.js
+++ js/js_practice.js
@@ -751,3 +751,22 @@ function clone(arr) {
 	arr.push([...arr])
 	return arr
 }
+
+// Burglary Series (04): Add its Name
+// Given three arguments ⁠— an object obj of the stolen items, the pets name and a value ⁠— return an object with that name and value in it (as key-value pairs).
+
+// Examples
+// addName({}, "Brutus", 300) ➞ { Brutus: 300 }
+
+// addName({ piano: 500 }, "Brutus", 400) ➞ { piano: 500, Brutus: 400 }
+
+// addName({ piano: 500, stereo: 300 }, "Caligula", 440) ➞ { piano: 500, stereo: 300, Caligula: 440 }
+// Notes
+// The value argument will be a number.
+
+function addName(obj, name, value) {
+	let wut = {};
+	   obj[name] = value;
+	   wut = obj;
+	   return wut;
+   }
