122b459090417394edb2109ded00cc44fa0b0009
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-10-20 08:51:24 -0500

    quick js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -718,3 +718,20 @@ function testJackpot(result) {
 function intWithinBounds(n, lower, upper) {
 	return n >= lower && n < upper && Number.isInteger(n);
 }
+
+// Frames Per Second
+// Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
+
+// Examples
+// frames(1, 1) ➞ 60
+
+// frames(10, 1) ➞ 600
+
+// frames(10, 25) ➞ 15000
+// Notes
+// FPS stands for "frames per second" and it's the number of frames a computer screen shows every second.
+
+function frames(minutes, fps) {
+	return (minutes * 60) * fps;
+}
+
