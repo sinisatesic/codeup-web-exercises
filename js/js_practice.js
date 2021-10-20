564c6695ac01dc1df1444ca562bae138dc8a6a7f
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-10-20 09:05:34 -0500

    js prob

--- js/js_practice.js
+++ js/js_practice.js
@@ -735,3 +735,19 @@ function frames(minutes, fps) {
 	return (minutes * 60) * fps;
 }
 
+// Clone an Array
+// The Code tab has a code which attempts to add a clone of an array to itself. There is no error message, but the results are not as expected. Can you fix the code?
+
+// Examples
+// clone([1, 1]) ➞ [1, 1, [1, 1]]
+
+// clone([1, 2, 3]) ➞ [1, 2, 3, [1, 2, 3]]
+
+// clone(["x", "y"]) ➞ ["x", "y", ["x", "y"]]
+// Notes
+// N/A
+
+function clone(arr) {
+	arr.push([...arr])
+	return arr
+}
