f5bfa955d7e1ca5dd5f677cf3593fc200bf277da
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-10-21 12:00:24 -0500

    js

--- js/js_practice.js
+++ js/js_practice.js
@@ -792,3 +792,35 @@ function addName(obj, name, value) {
 function coneVolume(h, r) {
 	return Number(((Math.PI*r**2*h)/3).toFixed(2));
 }
+
+// Length of a Nested Array
+// The .length property on an array will return the number of elements in the array. For example, the array below contains 2 elements:
+
+// [1, [2, 3]]
+// // 2 elements, number 1 and array [2, 3]
+// Suppose we instead wanted to know the total number of non-nested items in the nested array. In the above case, [1, [2, 3]] contains 3 non-nested items, 1, 2 and 3.
+
+// Write a function that returns the total number of non-nested items in a nested array.
+
+// Examples
+// getLength([1, [2, 3]]) ➞ 3
+
+// getLength([1, [2, [3, 4]]]) ➞ 4
+
+// getLength([1, [2, [3, [4, [5, 6]]]]]) ➞ 6
+
+// getLength([1, [2], 1, [2], 1]) ➞ 5
+// Notes
+// An empty array should return 0.
+
+function getLength(arr) {
+	let count = 0;
+	arr.forEach(item => {
+		if (Array.isArray(item)) {
+			count += getLength(item);
+		} else {
+			count++;
+		}
+	});
+	return count;
+}
