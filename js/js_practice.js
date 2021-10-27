78be32c5336daa562b927529ec3c6d95687b7760
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-10-27 08:15:55 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -905,3 +905,22 @@ function areaOfCountry(name, area) {
 	let clm = area/wlm*100;
 	return `${name} is ${(clm).toFixed(2)}% of the total world's landmass`;
 }
+
+// Let's Fuel Up!
+// A vehicle needs 10 times the amount of fuel than the distance it travels. However, it must always carry a minimum of 100 fuel before setting off.
+
+// Create a function which calculates the amount of fuel it needs, given the distance.
+
+// Examples
+// calculateFuel(15) ➞ 150
+
+// calculateFuel(23.5) ➞ 235
+
+// calculateFuel(3) ➞ 100
+// Notes
+// Distance will be a number greater than zero.
+// Return 100 if the calculated fuel turns out to be less than 100.
+
+function calculateFuel(n) {
+	return (n * 10) <= 100 ? 100 : n * 10;
+}
