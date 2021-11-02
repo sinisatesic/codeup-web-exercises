a19a810a70766d68a654f671a08a0b68588bda4c
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-11-02 09:13:18 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -971,3 +971,25 @@ function squareDigits(n) {
 	}
 	return Number(daNumz);
 }
+
+// Get Sum of People's Budget
+// Create the function that takes an array with objects and returns the sum of people's budgets.
+
+// Examples
+// getBudgets([
+//   { name: "John", age: 21, budget: 23000 },
+//   { name: "Steve",  age: 32, budget: 40000 },
+//   { name: "Martin",  age: 16, budget: 2700 }
+// ]) ➞ 65700
+
+// getBudgets([
+//   { name: "John",  age: 21, budget: 29000 },
+//   { name: "Steve",  age: 32, budget: 32000 },
+//   { name: "Martin",  age: 16, budget: 1600 }
+// ]) ➞ 62600
+// Notes
+// N/A
+
+function getBudgets(arr) {
+	return arr[0].budget + arr[1].budget + arr[2].budget;
+}
