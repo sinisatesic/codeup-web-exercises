2b34fbcf1e0bd7768fe90dcedc0a878b45c1b11f
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-11-09 08:38:40 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1165,3 +1165,23 @@ class Circle {
 let q = new Circle(4.44);
 console.log(q.getArea());
 console.log(q.getPerimeter());
+
+// Is the Number a Repdigit
+// A repdigit is a positive number composed out of the same digit. Create a function that takes an integer and returns whether it's a repdigit or not.
+
+// Examples
+// isRepdigit(66) ➞ true
+
+// isRepdigit(0) ➞ true
+
+// isRepdigit(-11) ➞ false
+// Notes
+// The number 0 should return true (even though it's not a positive number).
+// Check the Resources tab for more info on repdigits.
+
+function isRepdigit(num) {
+	let arr = num.toString().split('');
+	for (let i = 0; i < arr.length; i++){
+		return arr.every(x => x === arr[0])
+	}
+}
