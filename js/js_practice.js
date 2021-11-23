72b5bc5fb3cc4846f4e08eef14c1a686ad3d3957
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-11-23 10:21:11 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1302,3 +1302,23 @@ function num_of_digits(num) {
 function isSpecialArray(arr) {
 	return arr.every((a, b) => a % 2 === b % 2);
 }
+
+// Is the Number a Repdigit
+// A repdigit is a positive number composed out of the same digit. Create a function that takes an integer and returns whether it's a repdigit or not.
+
+// Examples
+// isRepdigit(66) ➞ true
+
+// isRepdigit(0) ➞ true
+
+// isRepdigit(-11) ➞ false
+// Notes
+// The number 0 should return true (even though it's not a positive number).
+// Check the Resources tab for more info on repdigits.
+
+function isRepdigit(num) {
+	let arr = num.toString().split('');
+	for (let i = 0; i < arr.length; i++){
+		return arr.every(x => x === arr[0])
+	}
+}
