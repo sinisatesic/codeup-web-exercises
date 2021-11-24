2b7b773bc7575d049e133a20a867009868116540
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-11-24 12:58:01 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1322,3 +1322,39 @@ function isRepdigit(num) {
 		return arr.every(x => x === arr[0])
 	}
 }
+
+// Parity Bit Validation
+// Parity bits are used as very simple checksum to ensure that binary data isn't corrupted during transit. Here's how they work:
+
+// If a binary string has an odd number of 1's, the parity bit is a 1.
+// If a binary string has an even number of 1's, the parity bit is a 0.
+// The parity bit is appended to the end of the binary string.
+// Create a function that validates whether a binary string is valid, using parity bits.
+
+// Worked Example
+// validateBinary("10110010") ➞ true
+
+// // The last digit is the parity bit.
+// // 0 is the last digit.
+// // 0 means that there should be an even number of 1's.
+// // There are four 1's.
+// // Return true.
+// Examples
+// validateBinary("00101101") ➞ true
+
+// validateBinary("11000000") ➞ true
+
+// validateBinary("11000001") ➞ false
+// Notes
+// All inputs will be a byte long (8 characters).
+// You can find another parity bit involved challenge via this link.
+
+function validateBinary(b) {
+	let count = 0;
+	for(let i = 0; i < b.length; i++){
+		if(b[i] % 2 == 1){
+			count++
+		}
+	}
+	return count % 2 === 0;
+}
