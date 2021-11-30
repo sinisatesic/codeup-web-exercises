829fcedd2fe6d200c27a91eb510cc8c8165d6501
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-11-30 12:03:21 -0600

    another js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1418,3 +1418,25 @@ function removeDups(arr) {
 function indexMultiplier(arr) {
 	return arr.reduce((accum, item, index) => accum + item * index, 0);
   }
+
+//   Sum of Number Elements in an Array
+// Arrays can be mixed with various types. Your task for this challenge is to sum all the number elements in the given array. Create a function that takes an array and returns the sum of all numbers in the array.
+
+// Examples
+// numbersSum([1, 2, "13", "4", "645"]) ➞ 3
+
+// numbersSum([true, false, "123", "75"]) ➞ 0
+
+// numbersSum([1, 2, 3, 4, 5, true]) ➞ 15
+// Notes
+// Check the Resources tab for help.
+
+function numbersSum(arr) {
+	let wut = 0;
+	for(let i = 0; i < arr.length; i++){
+		if (typeof arr[i] === 'number'){
+			wut += arr[i];
+		}
+	}
+	return wut;
+}
