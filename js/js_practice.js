beb6e2530d7332a6c23ea93e2c0178a18dbdda37
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-12-07 08:39:16 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1512,3 +1512,30 @@ function boxSeq(step) {
 	}
 	return r;
 }
+
+//
+
+// Match the Last Item
+// Create a function that takes an array of items and checks if the last item matches the rest of the array concatenated together.
+
+// Examples
+// matchLastItem(["rsq", "6hi", "g", "rsq6hig"]) ➞ true
+// // The last item is the rest joined.
+
+// matchLastItem([1, 1, 1, "11"]) ➞ false
+// // The last item should be "111".
+
+// matchLastItem([8, "thunder", true, "8thundertrue"]) ➞ true
+// Notes
+// The array is always filled with items.
+
+function matchLastItem(arr) {
+	let lastItem = arr.pop();
+	return lastItem === arr.reduce((a,b) => a.toString() + b.toString());
+}
+
+// bolje:
+
+function matchLastItem(arr) {
+	return arr.pop() === arr.join('');
+}
