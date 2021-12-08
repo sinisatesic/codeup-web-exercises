014438670b47c71c7c798b880044c9adb34c4f57
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-12-08 16:07:11 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1566,3 +1566,21 @@ function reverseWords(string) {
 function reverseWords(s) {
 	return s.split(" ").reverse().join(" ")
 }
+
+// Purge and Organize
+// Given an array of numbers, write a function that returns an array that...
+
+// Has all duplicate elements removed.
+// Is sorted from least to greatest value.
+// Examples
+// uniqueSort([1, 2, 4, 3]) ➞ [1, 2, 3, 4]
+
+// uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) ➞ [1, 2, 3, 4]
+
+// uniqueSort([6, 7, 3, 2, 1]) ➞ [1, 2, 3, 6, 7]
+// Notes
+// N/A
+
+function uniqueSort(arr) {
+	return [...new Set(arr)].sort((a,b) => a-b)
+}
