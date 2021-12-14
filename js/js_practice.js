8964e03ab1cef1d2df2f92cc55a5395dacafc8e9
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-12-14 14:23:42 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1656,3 +1656,35 @@ function mirror(arr) {
 	}
 	return arr
 }
+
+// Length of a Nested Array
+// The .length property on an array will return the number of elements in the array. For example, the array below contains 2 elements:
+
+// [1, [2, 3]]
+// // 2 elements, number 1 and array [2, 3]
+// Suppose we instead wanted to know the total number of non-nested items in the nested array. In the above case, [1, [2, 3]] contains 3 non-nested items, 1, 2 and 3.
+
+// Write a function that returns the total number of non-nested items in a nested array.
+
+// Examples
+// getLength([1, [2, 3]]) ➞ 3
+
+// getLength([1, [2, [3, 4]]]) ➞ 4
+
+// getLength([1, [2, [3, [4, [5, 6]]]]]) ➞ 6
+
+// getLength([1, [2], 1, [2], 1]) ➞ 5
+// Notes
+// An empty array should return 0.
+
+function getLength(arr) {
+	let count = 0;
+	arr.forEach(item => {
+		if (Array.isArray(item)) {
+			count += getLength(item);
+		} else {
+			count++;
+		}
+	});
+	return count;
+}
