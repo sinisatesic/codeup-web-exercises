1f2c82fb503f938cdfa42e9e0aa2826e01f07079
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-12-17 15:10:31 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1727,3 +1727,31 @@ function longestTime(h, m, s) {
 function longestTime(h, m, s) {
 	return h < m/60? m < s/60? s : m : h;
 }
+
+// Check if All Values Are True
+// Create a function that returns true if all parameters are truthy, and false otherwise.
+
+// Examples
+// allTruthy(true, true, true) ➞ true
+
+// allTruthy(true, false, true) ➞ false
+
+// allTruthy(5, 4, 3, 2, 1, 0) ➞ false
+// Notes
+// Falsy values include false, 0, "" (empty string), null, undefined, and NaN; everything else is truthy.
+// You will always be supplied with at least one parameter.
+
+function allTruthy(...args) {
+	return args.every((a) => 
+	a !== false && 
+	a !== 0 && 
+	a !== '' && 
+  	typeof a !== 'object' && 
+	a === a && 
+	a !== null && 
+	a !== undefined)
+}
+
+// lol:
+
+const allTruthy = (...args) => args.every(Boolean);
