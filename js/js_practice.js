fec8a6f53cda5d3d6001d8736c8b7e0835323d4f
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-12-21 09:07:40 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1755,3 +1755,28 @@ function allTruthy(...args) {
 // lol:
 
 const allTruthy = (...args) => args.every(Boolean);
+
+
+
+// Find the Mean of All Digits
+// Create a function that returns the mean of all digits.
+
+// Examples
+// mean(42) ➞ 3
+
+// mean(12345) ➞ 3
+
+// mean(666) ➞ 6
+// Notes
+// The mean of all digits is the sum of digits / how many digits there are (e.g. mean of digits in 512 is (5+1+2)/3(number of digits) = 8/3=2).
+// The mean will always be an integer.
+
+function mean(num) {
+	let splitted = num.toString().split('');
+	let arr = splitted.map(Number);
+	let wat = 0;
+	for(let i = 0; i < arr.length; i++){
+		wat += arr[i];
+	}
+	return wat/arr.length;
+}
