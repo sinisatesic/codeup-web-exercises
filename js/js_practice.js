51ba45cc260881131e6cbe57779f995076d63ed3
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-12-22 14:34:17 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1825,3 +1825,56 @@ const factorChain = arr => {
 		return arr[i]%arr[i-1]===0;
 	})
 }
+
+// Fix the Error: Value vs. Reference Types
+// Create a function that returns true if two arrays contain identical values, and false otherwise.
+
+// To solve this question, your friend writes the following code:
+
+// function checkEquals(arr1, arr2) {
+// if (arr1 === arr2) {
+//   return true
+//  } else {
+//   return false
+//  }
+// }
+// But testing the code, you see that something is not quite right. Running the code yields the following results:
+
+// checkEquals([1, 2], [1, 3]) ➞ false
+// // Good so far...
+
+// checkEquals([1, 2], [1, 2]) ➞ false
+// // Yikes! What happened?
+// Rewrite your friend's code so that it correctly checks if two arrays are equal. To be equal, the arrays must have the same elements in the same order. The tests below should pass:
+
+// Examples
+// checkEquals([1, 2], [1, 3]) ➞ false
+
+// checkEquals([1, 2], [1, 2]) ➞ true
+
+// checkEquals([4, 5, 6], [4, 5, 6]) ➞ true
+
+// checkEquals([4, 7, 6], [4, 5, 6]) ➞ false
+
+// checkEquals([4, 7, 6], [4, 6, 7]) ➞ false
+
+function checkEquals(arr1, arr2) {
+	if (arr1[0].toString() === arr2[0].toString()
+		 && arr1[1].toString() === arr2[1].toString()) {
+  	return true 
+  } else {
+  	return false
+  }
+}
+
+//bolji odgovor:
+
+function checkEquals(arr1, arr2) {
+	return arr1.every((x, i) => x == arr2[i])
+}
+
+//jos jedan:
+
+function checkEquals(arr1, arr2) {
+	return arr1.toString() == arr2.toString();
+}
