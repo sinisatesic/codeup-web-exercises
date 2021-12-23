3b62187543ab9799c8bc069a2b99436d824cf685
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2021-12-23 12:34:06 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -1889,3 +1889,21 @@ function getYear(yearString) {
 	}
 	return bucket.join('');
 }
+
+//
+
+// Capitalize the First Letter of Each Word
+// Create a function that takes a string as an argument and converts the first character of each word to uppercase. Return the newly formatted string.
+
+// Examples
+// makeTitle("This is a title") ➞ "This Is A Title"
+
+// makeTitle("capitalize every word") ➞ "Capitalize Every Word"
+
+// makeTitle("I Like Pizza") ➞ "I Like Pizza"
+
+// makeTitle("PIZZA PIZZA PIZZA") ➞ "PIZZA PIZZA PIZZA"
+
+function makeTitle(str) {
+	return str.split(' ').map((a) => a.charAt(0).toUpperCase() + a.substring(1)).join(' ');
+}
