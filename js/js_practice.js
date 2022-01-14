02c3d754f36057151b34b56427baf7caf1f2997c
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-01-14 13:08:28 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -2010,3 +2010,52 @@ function firstPlace(road) {
 	}
 	return str;
 }
+
+// Trace That Matrix
+// Given a square matrix (i.e. same number of rows as columns), its trace is the sum of the entries in the main diagonal (i.e. the diagonal line from the top left to the bottom right).
+
+// As an example, for:
+
+// [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ]
+// ... the trace is 1 + 5 + 9 = 15.
+
+// Write a function that takes a square matrix and computes its trace.
+
+// Examples
+// trace([
+//   [1, 4],
+//   [4, 1]
+// ]) ➞ 2
+
+// // 1 + 1 = 2
+
+// trace([
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ]) ➞ 15
+
+// // 1 + 5 + 9 = 15
+
+// trace([
+//   [1, 0, 1, 0],
+//   [0, 2, 0, 2],
+//   [3, 0, 3, 0],
+//   [0, 4, 0, 4]
+// ]) ➞ 10
+
+// // 1 + 2 + 3 + 4 = 10
+// Notes
+// As in the examples, the size of the matrices will vary (but they will always be square).]
+
+function trace(arr) {
+	let sum = 0;
+	for(let i = 0; i < arr.length; i++){
+		sum += arr[i][i];
+	}
+	return sum;
+}
