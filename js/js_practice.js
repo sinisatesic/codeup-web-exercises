8ef5059dcecd752d3df852682e9b3abadc107942
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-01-18 13:52:59 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -2059,3 +2059,47 @@ function trace(arr) {
 	}
 	return sum;
 }
+
+// Xs and Os, Nobody Knows
+// Create a function that takes a string, checks if it has the same number of x's and o's and returns either true or false.
+
+// Return a boolean value (true or false).
+// Return true if the amount of x's and o's are the same.
+// Return false if they aren't the same amount.
+// The string can contain any character.
+// When "x" and "o" are not in the string, return true.
+// Examples
+// XO("ooxx") ➞ true
+
+// XO("xooxx") ➞ false
+
+// XO("ooxXm") ➞ true
+// // Case insensitive.
+
+// XO("zpzpzpp") ➞ true
+// // Returns true if no x and o.
+
+// XO("zzoo") ➞ false
+// Notes
+// Remember to return true if there aren't any x's or o's.
+// Must be case insensitive.
+
+function XO(str) {
+	let xCount = 0;
+	let oCount = 0;
+	for(let i = 0 ;i < str.length; i++){
+		if (str[i] == "x" || str[i] == "X"){
+			xCount++
+		}
+		if (str[i] == "o" || str[i] == "O"){
+			oCount++
+		}
+	}
+	return xCount == oCount;
+}
+
+// bolji odgovor:
+
+function XO(str) {
+	return str.toLowerCase().split("x").length == str.split("o").length;
+  }
