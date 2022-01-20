cf89472c91ffce01c6d37366817d4584902ad1a9
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-01-20 08:06:53 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -2153,4 +2153,20 @@ function inBox(arr) {
 
 //bolji odgovor:
 
-const inBox = arr => arr.some(str => str.includes('*'));
+// const inBox = arr => arr.some(str => str.includes('*'));
+
+// Number to Reversed Array
+// Create a function that takes a number and returns an array with the digits of the number in reverse order.
+
+// Examples
+// reverseArr(1485979) ➞ [9, 7, 9, 5, 8, 4, 1]
+
+// reverseArr(623478) ➞ [8, 7, 4, 3, 2, 6]
+
+// reverseArr(12345) ➞ [5, 4, 3, 2, 1]
+// Notes
+// N/A
+
+function reverseArr(num) {
+	return [...String(num)].reverse().map((a) => parseInt(a));
+}
