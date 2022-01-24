4d283a3e79628993058c66157f4c5e5b5e41dc94
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-01-24 09:32:53 -0600

    js practice - hard

--- js/js_practice.js
+++ js/js_practice.js
@@ -2216,3 +2216,59 @@ function reverseArr(num) {
 // 	}
 // 	return final;
 // }
+
+// Multiplication Table
+// Your task, is to create N x N multiplication table, of size n provided in parameter.
+
+// For example, when n is 5, the multiplication table is:
+
+// 1, 2, 3, 4, 5
+// 2, 4, 6, 8, 10
+// 3, 6, 9, 12, 15
+// 4, 8, 12, 16, 20
+// 5, 10, 15, 20, 25
+// This example will result in:
+
+// [[1, 2, 3, 4, 5], [2, 4, 6, 8, 10], [3, 6, 9, 12, 15], [4, 8, 12, 16, 20], [5, 10, 15, 20, 25]]
+// Examples
+// multiplicationTable(1) ➞ [[1]]
+
+// multiplicationTable(3) ➞ [[1, 2, 3], [2, 4, 6], [3, 6, 9]]
+// Notes
+// N/A
+
+function multiplicationTable(n) {
+	let arr = [];
+	let secArr = [];
+	for(let i = 1; i <= n; i++){
+	  arr.push(i);
+	}
+	secArr.push(arr);
+	for(let j = 1; j < n; j++){
+	  if (secArr[j] !== arr[j]){
+		secArr.push(arr.map(a => a * (j + 1)))
+	  }
+	}
+	return secArr;
+  }
+
+  //kraci odgovor:
+
+  const multiplicationTable = n => 
+	Array.from({length: n}, (_, i) => 
+		Array.from({length: n},(_, j) => (i + 1) * (j + 1)));
+
+
+//kao moj odgovor al' bolji:
+
+function multiplicationTable(n) {
+	let res=[],temp=[];
+	for(let i=1;i<=n;i++){
+		for(let j=1;j<=n;j++){
+			temp.push(i*j);
+		}
+		res.push(temp);
+		temp=[];
+	}
+	return res;
+}
