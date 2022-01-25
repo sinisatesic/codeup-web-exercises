a0e71ac4763d9c5bc16ec38fdfeded0132c8ba37
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-01-25 15:44:05 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -2333,3 +2333,25 @@ function generation(x, y) {
   return gen[x][y]
 }
 
+
+// How Heavy Is It?
+// Given radius r and height h (in cm), calculate the mass of a cylinder when it's filled with water and the cylinder itself doesn't weigh anything. The desired output should be given in kg and rounded to two decimal places.
+
+// How to solve:
+
+// Calculate the volume of the cylinder.
+// Convert cm³ into dm³.
+// 1dm³ = 1L, 1L is 1Kg.
+// Examples
+// weight(4, 10) ➞ 0.5
+
+// weight(30, 60) ➞ 169.65
+
+// weight(15, 10) ➞ 7.07
+// Notes
+// If you get stuck on a challenge, find help in Resources.
+
+function weight(r, h) {
+	let volume = Math.PI * (r * r) * h;
+	return Number((volume/1000).toFixed(2));
+}
