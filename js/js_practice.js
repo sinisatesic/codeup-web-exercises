574feea3b25b67024318fb70e4247fcbfb2105f9
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-01-28 11:28:21 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -2355,3 +2355,21 @@ function weight(r, h) {
 	let volume = Math.PI * (r * r) * h;
 	return Number((volume/1000).toFixed(2));
 }
+
+// Lonely Integer
+// You are given an array of integers having both negative and positive values, except for one integer which can be negative or positive. Create a function to find out that integer.
+
+// Examples
+// lonelyInteger([1, -1, 2, -2, 3]) ➞ 3
+// // 3 has no matching negative appearance.
+
+// lonelyInteger([-3, 1, 2, 3, -1, -4, -2]) ➞ -4
+// // -4 has no matching positive appearance.
+
+// lonelyInteger([-9, -105, -9, -9, -9, -9, 105]) ➞ -9
+// Notes
+// N/A
+
+function lonelyInteger(arr) {
+	return arr.find((a) => !arr.includes(-a));
+}
