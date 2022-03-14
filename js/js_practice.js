5a481d2b8faba86ea9db7ab2b3ada87995002445
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-03-14 09:07:29 -0500

    js prob

--- js/js_practice.js
+++ js/js_practice.js
@@ -2449,3 +2449,19 @@ function findHighest( arr ) {
 // In order to work properly, the function should replace all "a"s with 4, "e"s with 3, "i"s with 1, "o"s with 0, and "s"s with 5.
 
 const hackerSpeak = (str) => str.replace(/a/g, '4').replace(/e/g, '3').replace(/i/g, '1').replace(/o/g, '0').replace(/s/g, '5');
+
+// Remove the Letters ABC
+// Create a function that will remove the letters "a", "b" and "c" from the given string and return the modified version. If the given string does not contain "a", "b", or "c", return null.
+
+// Examples
+// removeABC("This might be a bit hard") ➞ "This might e  it hrd"
+
+// removeABC("hello world!") ➞ null
+
+// removeABC("") ➞ null
+// Notes
+// If the given string does not contain "a", "b", or "c", return null.
+
+function removeABC(str) {
+	return !str.includes("a") && !str.includes("b") && !str.includes("c") ? null : str.replace(/[abc]/g, "");
+}
