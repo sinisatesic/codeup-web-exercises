0174f378dfebff47184f7d5158db3c6af19d062b
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-03-17 16:43:31 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -2465,3 +2465,31 @@ const hackerSpeak = (str) => str.replace(/a/g, '4').replace(/e/g, '3').replace(/
 function removeABC(str) {
 	return !str.includes("a") && !str.includes("b") && !str.includes("c") ? null : str.replace(/[abc]/g, "");
 }
+
+// Capitalize the Names
+// Create a function that takes an array of names and returns an array where only the first letter of each name is capitalized.
+
+// Examples
+// capMe(["mavis", "senaida", "letty"]) ➞ ["Mavis", "Senaida", "Letty"]
+
+// capMe(["samuel", "MABELLE", "letitia", "meridith"]) ➞ ["Samuel", "Mabelle", "Letitia", "Meridith"]
+
+// capMe(["Slyvia", "Kristal", "Sharilyn", "Calista"]) ➞ ["Slyvia", "Kristal", "Sharilyn", "Calista"]
+// Notes
+// Don't change the order of the original array.
+// Notice in the second example above, "MABELLE" is returned as "Mabelle".
+
+function capMe(arr) {
+	let arrTwo = [];
+	for(let i = 0; i < arr.length; i++){
+		arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].substr(1).toLowerCase();
+		arrTwo.push(arr[i]);
+	}
+	return arrTwo;
+}
+
+//bolji odgovor:
+
+function capMe(arr) {
+	return arr.map(x => x = x[0].toString().toUpperCase() + x.substring(1).toLowerCase());
+}
