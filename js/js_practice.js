397befac29817dbdb6a556d07459306e43e8edbf
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-03-24 09:22:17 -0500

    js prob 2 for day

--- js/js_practice.js
+++ js/js_practice.js
@@ -2537,3 +2537,19 @@ function backToHome(directions){
 
 	return n == s && e == w;;
 }
+
+// Find NaN in an Array
+// Create a function to find NaN in an array of numbers. The return value should be the index where NaN is found. If NaN is not found in the array, then return -1.
+
+// Examples
+// findNaN([1, 2, NaN]) ➞ 2
+
+// findNaN([NaN, 1, 2, 3, 4]) ➞ 0
+
+// findNaN([0, 1, 2, 3, 4]) ➞ -1
+// Notes
+// NaN will occur in the input array only once.
+
+function findNaN(number) {
+	return number.findIndex(Number.isNaN);
+}
