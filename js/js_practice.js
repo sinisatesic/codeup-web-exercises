ebd91eebd09cc95108cd21a77cd8f7ca1be31be4
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-03-25 16:38:50 -0500

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -2553,3 +2553,22 @@ function backToHome(directions){
 function findNaN(number) {
 	return number.findIndex(Number.isNaN);
 }
+
+// Find the Characters Counterpart Char Code
+// Create a function that takes a single character as an argument and returns the char code of its lowercased / uppercased counterpart.
+
+// Examples
+// Given that:
+//   - "A" char code is: 65
+//   - "a" char code is: 97
+
+// counterpartCharCode("A") ➞ 97
+
+// counterpartCharCode("a") ➞ 65
+// Notes
+// The argument will always be a single character.
+// Not all inputs will have a counterpart (e.g. numbers), in which case return the input's char code.
+
+function counterpartCharCode(char) {
+	return char == char.toUpperCase(0) ? char.toLowerCase().charCodeAt(0) : char.toUpperCase().charCodeAt(0);
+}
