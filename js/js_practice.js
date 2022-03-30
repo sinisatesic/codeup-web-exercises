98b365503fe99e408eec9cfa54b171990dab5cad
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-03-30 14:53:53 -0500

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -2605,3 +2605,44 @@ function evenlyDivisible(a, b, c) {
 	for (let i = a; i <= b; i++) if (i % c === 0) sum += i;
 	return sum;
   }
+
+//   War of Numbers
+// There's a great war between the even and odd numbers. Many numbers already lost their lives in this war and it's your task to end this. You have to determine which group sums larger: the evens, or the odds. The larger group wins.
+
+// Create a function that takes an array of integers, sums the even and odd numbers separately, then returns the difference between the sum of the even and odd numbers.
+
+// Examples
+// warOfNumbers([2, 8, 7, 5]) ➞ 2
+// // 2 + 8 = 10
+// // 7 + 5 = 12
+// // 12 is larger than 10
+// // So we return 12 - 10 = 2
+
+// warOfNumbers([12, 90, 75]) ➞ 27
+
+// warOfNumbers([5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243]) ➞ 168
+// Notes
+// The given array contains only positive integers.
+
+function warOfNumbers(arr) {
+	let evens = [], odds = [], evenSum = 0, oddSum = 0;
+	for(let i = 0; i < arr.length; i++){
+		if(arr[i]%2===0){evens.push(arr[i]);}
+		if(arr[i]%2===1){odds.push(arr[i]);}
+	}
+	evenSum = evens.reduce((a, b) => a + b, 0);
+	oddSum = odds.reduce((a, b) => a + b, 0);
+	if (evenSum > oddSum){
+		return evenSum - oddSum;
+	} else {
+		return oddSum - evenSum;
+	}
+}
+
+// bolji i siguarno brzi odgovor:
+
+const warOfNumbers = arr => {
+	const even = arr.filter(i=> i % 2 === 0).reduce((a,b)=>a+b,0)
+	const odd = arr.filter(i=> i % 2 === 1).reduce((a,b)=>a+b,0)
+	return odd > even ? odd - even : even - odd
+  }
