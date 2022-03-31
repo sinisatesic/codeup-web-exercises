65fc9281825003802293adef08110325d1a00835
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-03-31 16:13:15 -0500

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -2646,3 +2646,44 @@ const warOfNumbers = arr => {
 	const odd = arr.filter(i=> i % 2 === 1).reduce((a,b)=>a+b,0)
 	return odd > even ? odd - even : even - odd
   }
+
+//   Replace Vowel with Another Character
+// Create a function that takes a string and replaces the vowels with another character.
+
+// a = 1
+// e = 2
+// i = 3
+// o = 4
+// u = 5
+// Examples
+// replaceVowel("karachi") ➞ "k1r1ch3"
+
+// replaceVowel("chembur") ➞ "ch2mb5r"
+
+// replaceVowel("khandbari") ➞ "kh1ndb1r3"
+// Notes
+// The input will always be in lowercase.
+
+function replaceVowel(word) {
+	return word.replace(/a/g, '1')
+	.replace(/e/g, '2')
+	.replace(/i/g, '3')
+	.replace(/o/g, '4')
+	.replace(/u/g, '5');
+}
+
+// kul odgovor:
+
+function replaceVowel(word){
+	return word.replace(/[aeiou]/g,v=> "aeiou".indexOf(v)+1)
+}
+//drugi kul odgovor:
+function replaceVowel(word){
+	const vowels = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5};
+  	return [...word].map(i => i in vowels ? vowels[i] : i).join('');
+}
+//treci kul odgovor:
+const replacements = { a:1, e:2, i:3, o:4, u: 5}
+function replaceVowel(word){
+	return word.replace(/[aeiou]/g, el => replacements[el])
+}
