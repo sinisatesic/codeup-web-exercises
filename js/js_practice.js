f933ff1981011b4d87858da2d1a4c3335b001c1c
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-04-07 09:11:21 -0500

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -2749,3 +2749,28 @@ function numOfSubbarrays(arr) {
 		.filter(Array.isArray)
 		.length
 }
+
+//(hard):
+// Function Times 3
+// Create a function that takes three collections of arguments and returns the sum of the product of numbers.
+
+// Examples
+// product(1,2)(1,1)(2,3) ➞ 8
+// // 1 * 1 * 2 + 2 * 1 * 3
+
+// product(10,2)(5,0)(2,3) ➞ 100
+// // 10 * 5 * 2 + 2 * 0 * 3
+
+// product(1,2)(2,3)(3,4) ➞ 30
+// // 1 * 2 * 3 + 2 * 3 * 4
+
+// product(1,2)(0,3)(3,0) ➞ 0
+// // 1 * 0 * 3 + 2 * 3 * 0
+// Notes
+// All test input is valid.
+
+const product =
+      (...one) =>
+        (...two) =>
+          (...three) =>
+            (one[0]*two[0]*three[0]) + (one[1]*two[1]*three[1]);
