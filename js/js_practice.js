43d6375d5359282d559795eaccd63162c86f5ee6
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-04-11 12:21:12 -0500

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -2815,3 +2815,34 @@ function countOnes(i) {
 	}
 	return r;
   }
+
+//   ATM PIN Code Validation
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. Your task is to create a function that takes a string and returns true if the PIN is valid and false if it's not.
+
+// Examples
+// validatePIN("1234") ➞ true
+
+// validatePIN("12345") ➞ false
+
+// validatePIN("a234") ➞ false
+
+// validatePIN("") ➞ false
+// Notes
+// Some test cases contain special characters.
+// Empty strings must return false.
+
+function validatePIN(pin) {
+	if (pin.length !== 4 && pin.length !== 6){
+		return false
+	} else {
+		for(let i = 0; i < pin.length; i++){
+			return [...pin].every((a) => '0123456789'.includes(a));
+		}
+	}
+}
+
+// bolji odgovor:
+
+function validatePIN(pin) {
+	return pin.length !== 4 && pin.length !== 6 ? false : [...pin].every((a) => '0123456789'.includes(a));
+}
