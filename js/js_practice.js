713a21de3cc04b28c7597587fde291387ea954b5
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-04-13 13:00:43 -0500

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -2846,3 +2846,28 @@ function validatePIN(pin) {
 function validatePIN(pin) {
 	return pin.length !== 4 && pin.length !== 6 ? false : [...pin].every((a) => '0123456789'.includes(a));
 }
+
+// Even Index Elements in Array
+// Create a function that takes an array of integers and returns the sum of all the integers that have an even index, multiplied by the integer at the last index.
+
+// For example:
+
+// [2, 3, 4, 5] ➞ 30
+// (2 + 4) * 5 ➞ 30
+
+// [1, 4, 5, 6, 7, 2, 3] ➞ 48
+// (1 + 5 + 7 + 3) * 3 ➞ 48
+// Examples
+// evenLast([]) ➞ 0
+
+// evenLast([1, 3, 3, 1, 10]) ➞ 140
+
+// evenLast([-11, 3, 3, 1, 10]) ➞ 20
+// Notes
+// If the array is empty, return 0.
+
+function evenLast(arr) {
+	let evenNums = []
+	evenNums = arr.filter((el, index) => index % 2 === 0);
+return arr.length == 0 ? 0 : evenNums.reduce((a, b) => a + b, 0) * arr.pop();
+}
