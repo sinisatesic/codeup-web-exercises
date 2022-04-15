fe77d0da9a8217e7a43191bf6056113bed952948
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-04-15 15:56:28 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -2904,3 +2904,30 @@ function doubleLetters(word) {
 function doubleLetters(word) {
 	return /(\w)\1/.test(word)
 }
+
+// Return the Sum of the Two Smallest Numbers
+// Create a function that takes an array of numbers and returns the sum of the two lowest positive numbers.
+
+// Examples
+// sumTwoSmallestNums([19, 5, 42, 2, 77]) ➞ 7
+
+// sumTwoSmallestNums([10, 343445353, 3453445, 3453545353453]) ➞ 3453455
+
+// sumTwoSmallestNums([2, 9, 6, -1]) ➞ 8
+
+// sumTwoSmallestNums([879, 953, 694, -847, 342, 221, -91, -723, 791, -587]) ➞ 563
+
+// sumTwoSmallestNums([3683, 2902, 3951, -475, 1617, -2385]) ➞ 4519
+// Notes
+// Don't count negative numbers.
+// Floats and empty arrays will not be used in any of the test cases.
+
+function sumTwoSmallestNums(arr) {
+	let sorted = arr.sort((a, b) => a - Math.abs(b));
+	let noNegatives = sorted.filter((a) => a > -1);
+	let lowest = noNegatives[0];
+	let secondLowest = noNegatives[1];
+  return lowest + secondLowest;
+	}
+
+	//my readability here is A1
