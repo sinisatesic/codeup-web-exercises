d813b27b0696a92d3f4008d1ec86418f2327e72a
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-04-26 12:17:20 -0500

    hard lvl edabit, again

--- js/js_practice.js
+++ js/js_practice.js
@@ -3053,3 +3053,31 @@ const oddishOrEvenish = n => eval([...n+''].join`+`) % 2
 function aveSpd(upTime, upSpd, downSpd) {
 	return (2*(upSpd*downSpd))/(downSpd + upSpd)
 }
+
+// How Many Days Between Two Dates
+// Create a function that takes two dates and returns the number of days between the first and second date.
+
+// Examples
+// getDays(
+//   new Date("June 14, 2019"),
+//   new Date("June 20, 2019")
+// ) ➞ 6
+
+
+// getDays(
+//   new Date("December 29, 2018"),
+//   new Date("January 1, 2019")
+// ) ➞ 3
+// // Dates may not all be in the same month/year.
+
+
+// getDays(
+//   new Date("July 20, 2019"),
+//   new Date("July 30, 2019")
+// ) ➞ 10
+// Notes
+// N/A
+function getDays(date1, date2) {
+	let day = 24 * 60 * 60 * 1000;
+	return Math.round(Math.abs(date1 - date2)/day)
+}
