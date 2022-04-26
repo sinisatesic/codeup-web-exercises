70911eaf3a539996c5db9cdcaaac9a92bfcf4b15
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-04-26 09:06:08 -0500

    js prob - hard difficulty

--- js/js_practice.js
+++ js/js_practice.js
@@ -2999,6 +2999,30 @@ function isHarshad(n) {
 
 //jaci odgovor:
 
-const isHarshad = num =>
-	!(num % [...String(num)].reduce((x, y) => x + +y, 0))
+// const isHarshad = num =>
+// 	!(num % [...String(num)].reduce((x, y) => x + +y, 0))
 
+// 	Oddish vs. Evenish [Hard Difficulty]
+// 	Create a function that determines whether a number is Oddish or Evenish. A number is Oddish if the sum of all of its digits is odd, and a number is Evenish if the sum of all of its digits is even. If a number is Oddish, return "Oddish". Otherwise, return "Evenish".
+	
+// 	For example, oddishOrEvenish(121) should return "Evenish", since 1 + 2 + 1 = 4. oddishOrEvenish(41) should return "Oddish", since 4 + 1 = 5.
+	
+// 	Examples
+// 	oddishOrEvenish(43) ➞ "Oddish"
+// 	// 4 + 3 = 7
+// 	// 7 % 2 = 1
+	
+// 	oddishOrEvenish(373) ➞ "Oddish"
+// 	// 3 + 7 + 3 = 13
+// 	// 13 % 2 = 1
+	
+// 	oddishOrEvenish(4433) ➞ "Evenish"
+// 	// 4 + 4 + 3 + 3 = 14
+// 	// 14 % 2 = 0
+// 	Notes
+// 	N/A
+
+function oddishOrEvenish(num) {
+	return num.toString().split('').map(Number).reduce((a, b) => a + b, 0) % 2 !== 0 ? "Oddish" : "Evenish";
+}
+// boom!
