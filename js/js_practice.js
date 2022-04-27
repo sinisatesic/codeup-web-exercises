920ab13b7bd064d644a19dcc4ea70f8f24f0e698
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-04-27 12:46:35 -0500

    js

--- js/js_practice.js
+++ js/js_practice.js
@@ -3138,3 +3138,25 @@ const doesTriangleFit = (triangle, hole) => {
     [0, 1, 2].every(i => triangle[i] <= hole[i])
   );
 };
+
+// Exactly Three
+// You are given a number n. Determine whether n has exactly 3 divisors or not.
+
+// Examples
+// isExactlyThree(4) ➞ true
+// // 4 has only 3 divisors: 1, 2 and 4
+
+// isExactlyThree(12) ➞ false
+// // 12 has 6 divisors: 1, 2, 3, 4, 6, 12
+
+// isExactlyThree(25) ➞ true
+// // 25 has only 3 divisors: 1, 5, 25
+// Notes
+// 1 ≤ n ≤ 10^12
+
+function isExactlyThree(n) {
+	let sqrt = Math.sqrt(n)
+	for(let i = 2; i < sqrt; i++)
+		if(n % i === 0) return false
+	return Number.isInteger(sqrt) && n > 1
+}
