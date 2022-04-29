670c9cbf17a14ca8279b09e2b2a1e040c213fdc9
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-04-29 12:03:10 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -3209,3 +3209,27 @@ class Rectangle {
 	}
 }
 
+// Burglary Series (01): Calculate Total Losses
+// You just returned home to find your mansion has been robbed! Given an object of the stolen items, return the total amount of the burglary (number). If nothing was robbed, return the string "Lucky you!".
+
+// Examples
+// const stolenItems = {
+//   tv: 30,
+//   skate: 20,
+//   stereo: 50,
+// } ➞ 100
+
+// const stolenItems = {
+//   painting: 20000,
+// } ➞ 20000
+
+// const stolenItems = {} ➞ "Lucky you!"
+// Notes
+// The item value is always positive.
+// This is my first experience in creating a challenge. I started to learn testing so I decided to give it a go here at Edabit as well. Hope I did it right... Suggestions are very welcome!
+// This series is part of a collection that focuses on objects. 
+// If you are interested in following the breath-taking narrative skills of yours truly or just do some object focused challenges (the challenges are ordered in ascending difficulty order), you can more easily do that there.
+
+function calculateLosses(obj) {
+	return Object.keys(obj).length == 0 ? 'Lucky you!' : Object.values(obj).reduce((a, b) => a + b, 0);
+}
