65b3aa860197b2f72c3f7d8ed8040b8f98011379
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-05-02 14:27:57 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -3233,3 +3233,53 @@ class Rectangle {
 function calculateLosses(obj) {
 	return Object.keys(obj).length == 0 ? 'Lucky you!' : Object.values(obj).reduce((a, b) => a + b, 0);
 }
+
+
+var twoSum = function(nums, target) {
+    let lowestNumIndex = nums.findIndex(ele => ele === Math.min(...nums));
+	let sorted = nums.sort();
+	let second = 0;
+	let secondIndex = 0;
+	for(let i = 0; i < sorted.length; i++){
+		second = target - sorted[0];
+		secondIndex = sorted.indexOf(second);
+	}
+	return [lowestNumIndex, secondIndex];
+}
+
+
+// Temperature Conversion
+// Write a program that takes a temperature input in celsius and converts it to Fahrenheit and Kelvin. Return the converted temperature values in an array.
+
+// The formula to calculate the temperature in Fahrenheit from Celsius is:
+
+// F = C*9/5 +32
+// The formula to calculate the temperature in Kelvin from Celsius is:
+
+// K = C + 273.15
+// Examples
+// tempConversion(0) ➞ [32, 273.15]
+// // 0°C is equal to 32°F and 273.15 K.
+
+// tempConversion(100) ➞ [212, 373.15]
+
+// tempConversion(-10) ➞ [14, 263.15]
+
+// tempConversion(300.4) ➞ [572.72, 573.55]
+// Notes
+// Return calculated temperatures up to two decimal places.
+// Return "Invalid" if K is less than 0.
+
+function tempConversion(celsius) {
+	let f = 0, k = 0;
+	f = Number((celsius * 9/5 + 32).toFixed(2))
+	k = Number((celsius + 273.15).toFixed(2))
+	return k < 0 ? 'Invalid' : [f, k];
+}
+
+// bolji odgovor:
+
+function tempConversion(celsius) {
+	let result = [+(celsius*9/5 +32).toFixed(2), +(celsius + 273.15).toFixed(2)]
+	return result[1] < 0 ? 'Invalid' : result
+}
