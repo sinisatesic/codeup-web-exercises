ad11cc818ffeeec04c1123d9240e0f893dc9553f
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-05-03 08:58:11 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -3283,3 +3283,44 @@ function tempConversion(celsius) {
 	let result = [+(celsius*9/5 +32).toFixed(2), +(celsius + 273.15).toFixed(2)]
 	return result[1] < 0 ? 'Invalid' : result
 }
+
+// Switching Between Pencils
+// When coloring a striped pattern, you may start by coloring each square sequentially, meaning you spend time needing to switch coloring pencils.
+
+// Create a function where given an array of colors cols, return how long it takes to color the whole pattern. Note the following times:
+
+// It takes 1 second to switch between pencils.
+// It takes 2 seconds to color a square.
+// See the example below for clarification.
+
+// color_pattern_times(["Red", "Blue", "Red", "Blue", "Red"]) ➞ 14
+
+// // There are 5 colors so it takes 2 seconds to color each one (2 x 5 = 10).
+// // You need to switch the pencils 4 times and it takes 1 second to switch (1 x 4 = 4).
+// // 10 + 4 = 14
+// Examples
+// colorPatternTimes(["Blue"]) ➞ 2
+
+// colorPatternTimes(["Red", "Yellow", "Green", "Blue"]) ➞ 11
+
+// colorPatternTimes(["Blue", "Blue", "Blue", "Red", "Red", "Red"]) ➞ 13
+// Notes
+// Only change coloring pencils if the next color is different to the color before it.
+// Return a number in seconds.
+
+function colorPatternTimes(cols) {
+	let num = 0;
+	for(let i = 0; i < cols.length; i++){
+		num += 2;
+		if (cols[i] !== cols[i + 1]){
+			num += 1;
+		}
+	}
+	return num - 1;
+}
+
+// bolji odgovor:
+
+function colorPatternTimes(cl) {
+	return cl.length*2 + cl.slice(1).reduce((a,c,i)=>c===cl[i]?a:a+1,0)
+}
