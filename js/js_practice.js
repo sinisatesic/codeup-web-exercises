91c6157fc42e8a7567d720c0a9ff605c4909df07
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-05-20 16:43:42 -0500

    js prob

--- js/js_practice.js
+++ js/js_practice.js
@@ -3438,7 +3438,7 @@ function maskify(str) {
 
   var lengthOfLongestSubstring = function(s) {
     let max = 0;
-    let str = ‘’;
+    let str = '';
     for (let i = 0; i < s.length; i++) {
         if (str.indexOf(s[i]) === -1) {
             str += s[i];
@@ -3451,3 +3451,31 @@ function maskify(str) {
     }
     return max;
 };
+
+
+// Sastry Numbers
+// In this challenge, you have to establish if a given integer n is a Sastry number. If the number resulting from the concatenation of an integer n with its successor is a perfect square, then n is a Sastry Number.
+
+// Given a positive integer n, implement a function that returns true if n is a Sastry number, or false if it's not.
+
+// Examples
+// isSastry(183) ➞ true
+// // Concatenation of n and its successor = 183184
+// // 183184 is a perfect square (428 ^ 2)
+
+// isSastry(184) ➞ false
+// // Concatenation of n and its successor = 184185
+// // 184185 is not a perfect square
+
+// isSastry(106755) ➞ true
+// // Concatenation of n and its successor = 106755106756
+// // 106755106756 is a perfect square (326734 ^ 2)
+// Notes
+// A perfect square is a number with a square root equals to a whole integer.
+// You can expect only valid positive integers greater than 0 as input, without exceptions to handle. Zero is a perfect square, but the concatenation 00 isn't considered as a valid result to check.
+// In JavaScript, despite the specific challenge the results are proofed, the method used to calculate if an integer greater of 2 ** 53 - 1 is a Sastry number can lead to errors due to the approximation of the JS engine.
+
+function isSastry(number) {
+	let concatNum = "" + number + (number + 1);
+	return Math.sqrt(concatNum) % 1 === 0
+}
