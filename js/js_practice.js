a2a630f64ce84873eacd6b11c8c28d4867747248
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-06-02 11:40:33 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -3560,3 +3560,26 @@ function isPositiveDominant(a) {
 function isPositiveDominant(a) {
 	return new Set(a.filter(x=>x>0)).size>new Set(a.filter(x=>x<0)).size
 }
+
+// Combinations
+// Create a function that takes a variable number of arguments, each argument representing the number of items in a group. The function should return the number of permutations (combinations) of choices you would have if you selected one item from each group.
+
+// Examples
+// combinations(2, 3) ➞ 6
+
+// combinations(3, 7, 4) ➞ 84
+
+// combinations(2, 3, 4, 5) ➞ 120
+// Notes
+// Don't overthink this one.
+// Input may include the number zero.
+
+function combinations(...items) {
+	return items.filter(num => num > 0).reduce((a, b) => a * b);
+}
+
+// bolji odgovor:
+
+function combinations(items) {
+	return [...arguments].reduce((acc,item) => item===0 ? acc : acc*item)
+}
