19d34e9ea5dd1300df4c95090db9f7fd9f7645e7
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-06-03 13:44:53 -0500

    js prob - hard on edabit

--- js/js_practice.js
+++ js/js_practice.js
@@ -3583,3 +3583,38 @@ function combinations(...items) {
 function combinations(items) {
 	return [...arguments].reduce((acc,item) => item===0 ? acc : acc*item)
 }
+
+// Divide Array into Chunks
+// Write a function that divides an array into chunks of size n, where n is the length of each chunk.
+
+// Examples
+// chunkify([2, 3, 4, 5], 2) ➞ [[2, 3], [4, 5]]
+
+// chunkify([2, 3, 4, 5, 6], 2) ➞ [[2, 3], [4, 5], [6]]
+
+// chunkify([2, 3, 4, 5, 6, 7], 3) ➞ [[2, 3, 4], [5, 6, 7]]
+
+// chunkify([2, 3, 4, 5, 6, 7], 1) ➞ [[2], [3], [4], [5], [6], [7]]
+
+// chunkify([2, 3, 4, 5, 6, 7], 7) ➞ [[2, 3, 4, 5, 6, 7]]
+// Notes
+// It's O.K. if the last chunk is not completely filled (see example #2).
+// Integers will always be single-digit.
+
+function chunkify(arr, size) {
+	let newArr = [];
+	for(let i = 0; i < arr.length; i += size){
+		newArr.push(arr.slice(i, i + size));
+	}
+	return newArr;
+}
+
+// bolji odogvor:
+
+const chunkify = (arr, size) => {
+	let results = [];
+	while (arr.length) {
+		results.push(arr.splice(0, size));
+	}
+	return results;
+};
