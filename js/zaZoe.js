52ef5aa95b0ba12a1b10c652fffd260fad1d86cf
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-06-08 16:39:06 -0500

    personal project

--- js/zaZoe.js
+++ js/zaZoe.js
@@ -23,22 +23,22 @@ fnLetters.from("#first-name-Z", 25, {
 
 lnLetters.timeScale(70);
 
-fnLetters.from("#last-name-W", 9, {
+fnLetters.from("#last-name-W", 25, {
     delay: 0.1,
     opacity: 0,
     y: 5,
     ease: Expo.easeIn
-}).from("#last-name-O", 9, {
+}).from("#last-name-O", 25, {
     delay: 0.1,
     opacity: 0,
     y: 5,
     ease: Expo.easeIn
-}).from("#last-name-second-O", 9, {
+}).from("#last-name-second-O", 25, {
     delay: 0.1,
     opacity: 0,
     y: 5,
     ease: Expo.easeIn
-}).from("#last-name-D", 9, {
+}).from("#last-name-D", 25, {
     delay: 0.1,
     opacity: 0,
     z: 5,
@@ -59,18 +59,18 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";
+//     dots[slideIndex-1].className += " active";
+// }
