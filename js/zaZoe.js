bd378d71523fded7f110e52ad7bbc78ff4650e44
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-06-26 01:56:48 -0500

    additions to personal project

--- js/zaZoe.js
+++ js/zaZoe.js
@@ -1,21 +1,29 @@
 //greensock
+
+
+window.onload= () => {
+    let container = document.getElementById('mainContainer');
+    container.classList.add('fadey');
+}
+
+
 let fnLetters = new TimelineMax();
 let lnLetters = new TimelineMax();
 
 fnLetters.timeScale(70);
 
 fnLetters.from("#first-name-Z", 25, {
-    delay: 0.1,
+    delay: 2,
     opacity: 0,
     y: 5,
     ease: Expo.easeInOut
 }).from("#first-name-O", 25, {
-    delay: 0.1,
+    delay: 2,
     opacity: 0,
     y: 5,
     ease: Expo.easeInOut
 }).from("#first-name-E", 25, {
-    delay: 0.1,
+    delay: 2,
     opacity: 0,
     z: 5,
     ease: Expo.easeInOut
