c218df431507db63ed7cb621e847ef12ccdee94f
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-06-27 13:35:07 -0500

    js for book api project

---
+++ js/book.js
@@ -0,0 +1,29 @@
+
+
+// console.log('test');
+import { BOOK_API_KEY } from "./content";
+
+// const test_list = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${API_KEY}`
+// const test_list = `https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=${API_KEY}`
+const test_list = `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${BOOK_API_KEY}`
+
+
+// console.log(test_list)
+
+const testList = async (e) => {
+
+    const testResponse = await fetch(test_list);
+    const jsonTest = await testResponse.json();
+
+    console.log(jsonTest);
+    console.log(jsonTest.results)
+
+    jsonTest.results.forEach(book => {
+        // console.log(book)
+        console.log(book.author)
+        //
+        // console.log(test)
+    })
+}
+
+testList();
