6edb302821f900c746e7a3b1770483208546f98a
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2022-06-27 21:47:34 -0500

    committing separate client side until key hidden server side

---
+++ js/books_client.js
@@ -0,0 +1,22 @@
+const test_list = `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${BOOK_API_KEY}`
+
+
+// console.log(test_list)
+
+const testList = async (e) => {
+
+    const testResponse = await fetch(test_list);
+    const jsonTest = await testResponse.json();
+
+    console.log(jsonTest);
+    console.log(jsonTest.results)
+
+    jsonTest.results.forEach(book => {
+        // console.log(book)
+        console.log(book.author)
+        //
+        // console.log(test)
+    })
+}
+
+testList();
