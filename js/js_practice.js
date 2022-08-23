eba3aeb639169e3d83f5265655642634842a1092
sinisatesic <sinisa.tesic210@gmail.com>
2022-08-23 11:04:05 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -3911,3 +3911,32 @@ const greet = (nameOne, nameTwo) => {
 // Happy path encompasses two argument option within nested if statement.
 // happy path of nested if will provide output if two names provided, while said path for nested targets one argument input.
 // 
+
+
+// Currying Functions
+// Mubashir was reading about currying functions. He needs your help to multiply an array of numbers using currying functions.
+
+// Create a function which takes a array arr of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+// Examples
+// multiply([1, 2, 3])(2) ➞ [2, 4, 6]
+
+// multiply([4, 6, 5])(10) ➞ [40, 60, 50]
+
+// multiply([1, 2, 3])(0) ➞ [0, 0, 0]
+// Notes
+// Your function must be called multiply.
+
+//odgovor:
+
+function multiply (arr){
+	return function factor(int){
+		return arr.map(x => x * int)
+	}
+}
+
+// bolji odgovor:
+
+multiply = arr => a => arr.map(e => e * a)
