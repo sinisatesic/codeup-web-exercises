d49776206abed4fded87a2e8d923eeb7500d490f
sinisatesic <sinisa.tesic210@gmail.com>
2022-09-20 10:55:58 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -4161,3 +4161,22 @@ function allAboutStrings(str) {
 		   ) return false;
 		return true;
 	  }
+
+
+// 	  Converting One Binary String to Another
+// Write a function that returns the minimum number of swaps to convert the first binary string into the second.
+
+// Examples
+// minSwaps("1100", "1001") ➞ 1
+
+// minSwaps("110011", "010111") ➞ 1
+
+// minSwaps("10011001", "01100110") ➞ 4
+// Notes
+// Both binary strings will be of equal length.
+// Both binary strings will have an equal number of zeroes and ones.
+// A swap is switching two elements in a string (swaps do not have to be adjacent).
+
+function minSwaps(s1, s2) {
+	return s1.split('').filter((a,b) => a !== s2[b]).length/2
+}
