ebf074910088128b427d0999c048299134d79734
sinisatesic <sinisa.tesic210@gmail.com>
2022-09-22 14:37:08 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -4201,3 +4201,67 @@ function isPandigital(num) {
 	
 	return target.every((a) => noDups.includes(a));
 }
+
+//drugaci odgovor:
+
+function isPandigital(num) {
+	return new Set(num.toString().split('')).size===10
+}
+
+//jos jedan odgovor sa for loop:
+
+function isPandigital(num) {
+	const numStr = num.toString();
+	let flag = true;
+	for (let i=0; i < 10; i++) {
+		if (!numStr.includes(i.toString())) {
+			flag = false;
+		}
+	}
+	return flag;
+}
+
+
+// The Frugal Gentleman
+// Atticus has been invited to a dinner party, and he decides to purchase a bottle of wine. However, he has little knowledge of how to choose a good bottle. Being a very frugal gentleman (yet disliking looking like a cheapskate), he decides to use a very simple rule. In any selection of two or more wines, he will always buy the second-cheapest.
+
+// Given an array of wine objects, write a function that returns the name of the wine he will buy for the party. If given an empty array, return null. If given an array of only one, Atticus will buy that wine.
+
+// Examples
+// chosenWine([
+//   { name: "Wine A", price: 8.99 },
+//   { name: "Wine 32", price: 13.99 },
+//   { name: "Wine 9", price: 10.99 }
+// ]) ➞ "Wine 9"
+
+// chosenWine([{ name: "Wine A", price: 8.99 }]) ➞ "Wine A"
+
+// chosenWine([]) ➞ null
+// Notes
+// All wines will be different prices, so there is no confusion in the ordering.
+
+function chosenWine(wines) {
+	if (wines.length === 0){
+		return null
+	}
+	if (wines.length === 1){
+		return wines[0].name;
+	}
+	if (wines.length > 1){
+		let sorted = wines.sort((a,b) => a.price - b.price);
+		return sorted[1].name;
+	}
+}
+
+// cisci odgovor:
+
+function chosenWine(wines) {
+	if (wines.length === 0) return null
+	if (wines.length === 1) return wines[0].name
+	return wines.sort((a,b) => a.price - b.price)[1].name
+}
+
+const chosenWine = (wines) => wines.length === 0
+? null
+: wines.length === 1
+? wines[0].name : wines.sort((a,b) => a.price - b.price)[1].name;
