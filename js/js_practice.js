705526094bc13d7f488de2d76d121b489467b64e
sinisatesic <sinisa.tesic210@gmail.com>
2022-10-09 17:38:44 -0500

    js practice leetcode easy lvl

--- js/js_practice.js
+++ js/js_practice.js
@@ -4626,3 +4626,48 @@ function isLeap(year) {
 	return new Date(year, 1, 29).getDate() === 29;
   }
   // checks if February has 29 days
+
+//   9. Palindrome Number
+// Easy
+
+// 7460
+
+// 2321
+
+// Add to List
+
+// Share
+// Given an integer x, return true if x is palindrome integer.
+
+// An integer is a palindrome when it reads the same backward as forward.
+
+// For example, 121 is a palindrome while 123 is not.
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+ 
+
+// Follow up: Could you solve it without converting the integer to a string?
+
+var isPalindrome = function(x) {
+    return x.toString() === x.toString().split('').reverse().join('');
+};
