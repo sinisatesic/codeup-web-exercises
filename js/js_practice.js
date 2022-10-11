401db27a368573e917f09b0fa89f0bae4b76f276
sinisatesic <sinisa.tesic210@gmail.com>
2022-10-11 11:48:58 -0500

    leet code easy js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -4671,3 +4671,53 @@ function isLeap(year) {
 var isPalindrome = function(x) {
     return x.toString() === x.toString().split('').reverse().join('');
 };
+
+const isPalindrome = (x) => x.toString().split('').reverse().join('') === x.toString('');
+
+
+// 58. Length of Last Word
+// Easy
+
+// 1894
+
+// 121
+
+// Add to List
+
+// Share
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of only English letters and spaces ' '.
+// There will be at least one word in s.
+// Accepted
+// 891,119
+// Submissions
+
+var lengthOfLastWord = function(s) {
+    let arr = s.trim().split(" ");
+    return arr[arr.length - 1].length;
+};
