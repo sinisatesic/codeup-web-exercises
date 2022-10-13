f5e6dc08788b56dad788164f069a59a92c47b056
sinisatesic <sinisa.tesic210@gmail.com>
2022-10-13 12:47:33 -0500

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -4796,3 +4796,56 @@ var addBinary = function(str1, str2) {
    }
    return res.reverse().join('');
 };
+
+// 83. Remove Duplicates from Sorted List
+// Easy
+
+// 6023
+
+// 212
+
+// Add to List
+
+// Share
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+// Example 2:
+
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 300].
+// -100 <= Node.val <= 100
+// The list is guaranteed to be sorted in ascending order.
+
+const deleteDuplicates = (head) => {
+    if(!head) {
+        return null;
+    }
+
+    let currNode = head;
+
+    while(currNode?.next) {
+        let nextNode = currNode.next;
+
+        while(nextNode && nextNode.val === currNode.val) {
+            nextNode = nextNode.next;
+        }
+
+        currNode.next = nextNode;
+        currNode = nextNode;
+    }
+
+    return head;
+};
