34524ceaa136b9f43b5dae97689e29a3087b4050
sinisatesic <sinisa.tesic210@gmail.com>
2022-10-18 14:22:46 -0500

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -4849,3 +4849,50 @@ const deleteDuplicates = (head) => {
 
     return head;
 };
+
+// Frequency Distribution
+// Create a function that returns the frequency distribution of an array. This function should return an object, where the keys are the unique elements and the values are the frequency in which those elements occur.
+
+// Examples
+// getFrequencies(["A", "B", "A", "A", "A"]) ➞ { A: 4, B: 1 }
+
+// getFrequencies([1, 2, 3, 3, 2]) ➞ { "1": 1, "2": 2, "3": 2 }
+
+// getFrequencies([true, false, true, false, false]) ➞ { true: 2, false: 3 }
+
+// getFrequencies([]) ➞ {}
+// Notes
+// If given an empty array, return an empty object (see example #4).
+// The object should be in the same order as in the input array.
+
+function getFrequencies(arr) {
+	let obj = {};
+		for(let i = 0; i < arr.length; i++){
+			let el = arr[i];
+			if(obj[el] !== undefined){
+			obj[el] += 1;
+		} else {
+			obj[el] = 1;
+		}
+	}
+	return obj
+}
+
+// bolji odgovor:
+
+const getFrequencies = arr =>
+  arr.reduce((map, item) => {
+    map[item] = map[item] + 1 || 1;
+    return map;
+  }, {});
+
+  // jos jedan odgovor:
+
+  function getFrequencies(arr) {
+	let out = {};
+	for (let el of arr) {
+		if (out[el]) out[el] += 1;
+		else out[el] = 1;
+	}
+	return out;
+}
