6ea2e300a992a39a742f281822f4f23160c997d3
sinisatesic <sinisa.tesic210@gmail.com>
2022-11-03 13:13:20 -0500

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -5000,3 +5000,43 @@ function doubleSwap(str, c1, c2) {
 		return character
 	}).join('')
 }
+
+//Layers in a Rug
+// Write a function that counts how many concentric layers a rug has.
+
+// Examples
+// countLayers([
+//   "AAAA",
+//   "ABBA",
+//   "AAAA"
+// ]) ➞ 2
+
+// countLayers([
+//   "AAAAAAAAA",
+//   "ABBBBBBBA",
+//   "ABBAAABBA",
+//   "ABBBBBBBA",
+//   "AAAAAAAAA"
+// ]) ➞ 3
+
+// countLayers([
+//   "AAAAAAAAAAA",
+//   "AABBBBBBBAA",
+//   "AABCCCCCBAA",
+//   "AABCAAACBAA",
+//   "AABCADACBAA",
+//   "AABCAAACBAA",
+//   "AABCCCCCBAA",
+//   "AABBBBBBBAA",
+//   "AAAAAAAAAAA"
+// ]) ➞ 5
+// Notes
+// Multiple layers can share the same component so count them separately (example #2).
+// Layers will be horizontally and vertically symmetric.
+// There will be at least one layer for each rug.
+
+// nice:
+
+function countLayers(rug) {
+	return [...new Set(rug)].length;
+}
