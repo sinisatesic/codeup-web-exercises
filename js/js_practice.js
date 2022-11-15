095c2f8bd292b39b1fe47d0ea196fb9c6b2171a7
sinisatesic <sinisa.tesic210@gmail.com>
2022-11-15 08:43:34 -0600

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -5186,3 +5186,82 @@ function identicalFilter(arr) {
 function identicalFilter(arr){
     return arr.filter((str) => new Set(str).size === 1)
 }
+
+// breakdown of how Set works:
+
+const ages = [26, 27, 26, 26, 28, 28, 29, 29, 30]
+
+const temp = {};
+for (const age of ages) {
+  temp[age] = true;
+}
+
+const uniqueAges = [];
+for (const age in temp) {
+  uniqueAges.push(age);
+}
+
+console.log(uniqueAges);
+
+// Check for Anagrams
+// Create a function that takes two strings and returns either true or false depending on whether they're anagrams or not.
+
+// Examples
+// isAnagram("cristian", "Cristina") ➞ true
+
+// isAnagram("Dave Barry", "Ray Adverb") ➞ true
+
+// isAnagram("Nope", "Note") ➞ false
+// Notes
+// Should be case insensitive.
+// The two given strings can be of different lengths.
+
+function isAnagram(s1, s2) {
+	return s1.toLowerCase().split('').sort().join('') ===
+		s2.toLowerCase().split('').sort().join('');
+}
+
+// better format:
+
+function isAnagram(s1, s2) {
+	var t = s => s.toLowerCase().split('').sort().join('');
+	return t(s1) === t(s2); 
+  }
+
+  // with prototype:
+
+  function isAnagram(s1, s2) {
+	return s1.alphabetize() == s2.alphabetize();
+  }
+  
+  String.prototype.alphabetize = function(){
+	return this.toLowerCase().split('').sort().join('');
+  }
+
+//   Last Digit Ultimate
+// Your job is to create a function, that takes 3 numbers: a, b, c and returns true if the last digit of a * b = the last digit of c. Check the examples below for an explanation.
+
+// Examples
+// lastDig(25, 21, 125) ➞ true
+// // The last digit of 25 is 5, the last digit of 21 is 1, and the last
+// // digit of 125 is 5, and the last digit of 5*1 = 5, which is equal
+// // to the last digit of 125(5).
+
+// lastDig(55, 226, 5190) ➞ true
+// // The last digit of 55 is 5, the last digit of 226 is 6, and the last
+// // digit of 5190 is 0, and the last digit of 5*6 = 30 is 0, which is
+// // equal to the last digit of 5190(0).
+
+// lastDig(12, 215, 2142) ➞ false
+// // The last digit of 12 is 2, the last digit of 215 is 5, and the last
+// // digit of 2142 is 2, and the last digit of 2*5 = 10 is 0, which is
+// // not equal to the last digit of 2142(2).
+// Notes
+// Numbers can be negative.
+
+function lastDig(a, b, c) {
+	let strA = a.toString().split(''), strB = b.toString().split(''), strC = c.toString().split('');
+	let productAB = strA[strA.length - 1] * strB[strB.length - 1];
+	let splitProduct = productAB.toString().split('');
+	return Number(splitProduct[splitProduct.length - 1]) === Number(strC[strC.length - 1]);
+}
