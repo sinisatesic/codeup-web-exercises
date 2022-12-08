a194931e6b67fbf1d9e2fb66458e5f164215e0eb
sinisatesic <sinisa.tesic210@gmail.com>
2022-12-08 11:01:16 -0600

    js leetcode prob

--- js/js_practice.js
+++ js/js_practice.js
@@ -5338,3 +5338,59 @@ function parseRoman(s) {
         return r + (val[a] < val[aa[i + 1]] ? -val[a] : val[a]);
     }, 0);
 }
+
+
+// leetcode prob:
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+ 
+
+// Follow-up: Could you solve the problem in linear time and in O(1) space?
+// Accepted
+// 1.5M
+// Submissions
+// 2.4M
+// Acceptance Rate
+// 63.9%
+
+var majorityElement = function(nums) {
+    let counts = {}, maj = 0;
+
+    for (const num of nums) {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+    maj = Object.keys(counts).reduce((a, b) => counts[a] > counts[b] ? a : b);
+    }
+    return maj;
+};
+
+// way faster:
+
+var majorityElement = function(nums) {
+    const n = nums.length
+    const map = new Map()
+    for (let i = 0; i < n; i++) {
+        map.set(nums[i], map.has(nums[i]) ? map.get(nums[i]) + 1 : 1)
+        if (map.get(nums[i]) > (n / 2)) return nums[i]
+    }    
+    return -1
+};
