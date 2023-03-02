a5f59ef91d1c169f5837e0812e0b0acf103b1849
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2023-03-02 13:58:57 -0600

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -5700,3 +5700,28 @@ function dashed(str) {
 	}
 	return newStr
 }
+
+// Snail Goes Up the Stairs
+// A snail goes up the stairs. Every step, he must go up the step, then go across to the next step. He wants to reach the top of the tower.
+
+// Write a function that returns the distance the snail must travel to the top of the tower given the height and length of each step and the height of the tower.
+
+// Examples
+// totalDistance(0.2, 0.4, 100.0) ➞ 300.0
+// // Total distance is 300.
+
+// totalDistance(0.3, 0.2, 25.0) ➞ 41.7
+
+// totalDistance(0.1, 0.1, 6.0) ➞ 12.0
+// Notes
+// All given values are greater than 0.
+// Round answers to the nearest tenth 0.1.
+// Number of steps determined by tower height divided by stair height.
+// For the purposes of this exercise, the last step's length must be counted to complete the journey.
+
+function totalDistance(height, length, tower) {
+	let steps = 0, ans = 0;
+	steps = tower/height;
+	ans = (height + length) * steps
+	return Number(ans.toFixed(1))
+}
