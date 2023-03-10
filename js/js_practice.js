9372dc3742b0793584b12a72fa8b8f016c2b7cf7
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2023-03-10 11:46:31 -0600

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -5727,3 +5727,34 @@ function totalDistance(height, length, tower) {
 }
 
 //
+
+// Measure the Depth of Emptiness
+// In this challenge you will receive an input of the form:
+
+// [[[[[[[[[[[]]]]]]]]]]]
+// In other words, an array containing an array containing an array containing... an array containing nothing.
+
+// Your goal is to measure the depth of this array, where [] has a depth 1, [[]] has depth of 2, [[[]]] has depth 3, etc.
+
+// Examples
+// measureDepth([]) ➞ 1
+
+// measureDepth([[]]) ➞ 2
+
+// measureDepth([[[]]]) ➞ 3
+
+// measureDepth([[[[[[[[[[[]]]]]]]]]]]) ➞ 11
+// Notes
+// For a bonus challenge, try to find a solution without recursion.
+
+function measureDepth(arr) {
+	let string = JSON.stringify(arr);
+	let splitted = string.split('');
+	let counter = 0;
+	for(let i = 0; i < splitted.length; i++){
+		if (splitted[i] == '['){
+			counter++
+		}
+	}
+	return counter;
+}
