28ac73a33b899bdca6fb51921b7c75594bf0f18f
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2023-03-22 09:29:27 -0500

    js prac

--- js/js_practice.js
+++ js/js_practice.js
@@ -5783,7 +5783,48 @@ const measureDepth = arr => JSON.stringify(arr).length / 2;
 function formatMath(expr) {
 	expr = expr.replace('x', '*');
 	let ans = eval(expr);
-	let splitted = expr.split(' ');
 	let final = `${expr} = ${ans}`
 	return final.replace('*', 'x')
+}
+
+// Neutralisation
+// Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+
+// When positives and positives interact, they remain positive.
+// When negatives and negatives interact, they remain negative.
+// But when negatives and positives interact, they become neutral, and are shown as the number 0.
+// Worked Example
+// neutralise("+-+", "+--") ➞ "+-0"
+// // Compare the first characters of each string, then the next in turn.
+// // "+" against a "+" returns another "+".
+// // "-" against a "-" returns another "-".
+// // "+" against a "-" returns "0".
+// // Return the string of characters.
+// Examples
+// neutralise("--++--", "++--++") ➞ "000000"
+
+// neutralise("-+-+-+", "-+-+-+") ➞ "-+-+-+"
+
+// neutralise("-++-", "-+-+") ➞ "-+00"
+// Notes
+// The two strings will be the same length.
+
+function neutralise(s1, s2) {
+	let s1Arr = [...s1], s2Arr = [...s2], ans = [];
+	while(s1Arr.length !== 0 && s2Arr.length !== 0){
+		if (s1Arr[0] == s2Arr[0]){
+			ans.push(s1Arr[0])
+		} else {
+			ans.push('0')
+		}
+		s1Arr.shift();
+		s2Arr.shift();
+	}
+	return ans.join('');
+}
+
+//bolji odgovor:
+
+function neutralise(s1, s2) {
+	return [...s1].map((i, idx) => i === s2[idx] ? i : '0').join('');
 }
