eba1f65cb2cd464bd51b1f35428559da7c289ec5
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2023-08-10 11:44:20 -0500

    js problem

--- js/js_practice.js
+++ js/js_practice.js
@@ -5935,3 +5935,23 @@ function mubashirFunction(a, b){
   const c = a => String(a).split('').reduce((a,b)=>Number(a)+Number(b))
 	return c(a)*c(b)
 	}
+
+// 	How Many Rectangles?
+// Let there be a square matrix, where each square is a rectangle, and a combination of more squares are also rectangles. To find the number of rectangles, Pete sat down and started counting... but that's highly inefficient.
+
+// Create a function that takes the order of the matrix as input and returns the number of rectangles in them.
+
+// Examples
+// rectangles(1) ➞ 1
+
+// rectangles(2) ➞ 9
+
+// rectangles(3) ➞ 36
+// Notes
+// The input will always be an integer.
+// Number of rectangles are given by: ((n(n+1))/2)^2
+// Watch the video listed in the Resources tab to get three different formulas.
+
+function rectangles(step) {
+	return Math.pow((step * (step+1))/2, 2)
+}
