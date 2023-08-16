cfe5d1df92d6b4830326b7c66f9fa17d3d6e5f4f
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2023-08-16 16:12:33 -0500

    js problem

--- js/js_practice.js
+++ js/js_practice.js
@@ -5981,3 +5981,32 @@ function reverseImage(image) {
 // nize:
 
 const reverseImage = image => image.map(row => row.map(num => Number(!num)));
+
+// Positive Count / Negative Sum
+// Create a function that takes an array of positive and negative numbers. Return an array where the first element is the count of positive numbers and the second element is the sum of negative numbers.
+
+// Examples
+// countPosSumNeg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]) ➞ [10, -65]
+// // There are a total of 10 positive numbers.
+// // The sum of all negative numbers equals -65.
+
+// countPosSumNeg([92, 6, 73, -77, 81, -90, 99, 8, -85, 34]) ➞ [7, -252]
+
+// countPosSumNeg([91, -4, 80, -73, -28]) ➞ [2, -105]
+
+// countPosSumNeg([]) ➞ []
+// Notes
+// If given an empty array, return an empty array: []
+// 0 is not positive.
+
+function countPosSumNeg(arr) {
+	let count = 0, sum = 0, ans = [];
+	for(let i = 0; i < arr.length; i++){
+		if (arr[i] < 0){
+			sum += arr[i]
+		} else {
+			count++
+		}
+	}
+	return arr.length == 0 ? [] : [count, sum]
+}
