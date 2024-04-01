4c76df708085f0ae757b699545418e81bf756416
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2024-04-01 10:21:23 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -6371,3 +6371,33 @@ function addParityBit(b) {
 	var hold = a ? 0 : 1
 	return b + hold 
 }
+
+// LCM of Two Numbers
+// Write a function that returns the least common multiple (LCM) of two integers.
+
+// Examples
+// lcm(9, 18) ➞ 18
+
+// lcm(8, 5) ➞ 40
+
+// lcm(17, 11) ➞ 187
+// Notes
+// Both values will be positive.
+// The LCM is the smallest integer that is divisible by both numbers such that the remainder is zero.
+
+function lcm(n1, n2) {
+	if ((typeof n1 !== 'number') || (typeof n2 !== 'number')) 
+	 return false;
+   return (!n1 || !n2) ? 0 : Math.abs((n1 * n2) / gcd(n1, n2));
+ }
+ 
+ function gcd(n1, n2) {
+   n1 = Math.abs(n1);
+   n2 = Math.abs(n2);
+   while(n2) {
+	 var newNum = n2;
+	 n2 = n1 % n2;
+	 n1 = newNum;
+	 }
+	 return n1;
+   }
