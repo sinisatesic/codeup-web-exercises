34fc1d52f573ff31aefba357eb39a101496eda36
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2024-05-14 15:41:07 -0500

    practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -6491,3 +6491,52 @@ const toBinary = (input) => {
 function sortDrinkByPrice(drinks) {
 	return drinks.sort((a, b) => a.price > b.price ? 1 : -1);
 }
+
+// Create a method in the Person class which returns how another person's age compares. Given the instances p1, p2 and p3, which will be initialised with the attributes name and age, return a sentence in the following format:
+
+// {other person name} is {older than / younger than / the same age as} me.
+
+// Examples
+// p1 = Person("Samuel", 24)
+// p2 = Person("Joel", 36)
+// p3 = Person("Lily", 24)
+// p1.compareAge(p2) ➞ "Joel is older than me."
+
+// p2.compareAge(p1) ➞ "Samuel is younger than me."
+
+// p1.compareAge(p3) ➞ "Lily is the same age as me."
+// Notes
+// Check out the Resources tab for some helpful tutorials on JavaScript classes!
+// If you're really stuck, check out the Solutions tab for answers.
+
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+		
+	compareAge(other) {
+		if (this.age < other.age){
+			return `${other.name} is older than me.`
+		}else if (this.age > other.age){
+			return `${other.name} is younger than me.`
+		}else{
+			return `${other.name} is the same age as me.`
+		}
+	}
+}
+
+//
+
+class Person {
+	constructor(name, age) {
+		this.name = name
+		this.age = age
+	}
+		
+	compareAge(other) {
+		let i = Math.sign(other.age - this.age),
+				x = ["the same age as", "older than"][i] || "younger than";
+		return `${other.name} is ${x} me.`;
+	}
+}
