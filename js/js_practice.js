c0bfa8f1ae6359943db3230a51f76aa532f14955
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2024-05-15 20:49:39 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -6540,3 +6540,37 @@ class Person {
 		return `${other.name} is ${x} me.`;
 	}
 }
+
+// Hashtag Generator
+// Create a function that is a Hashtag Generator by using the following rules:
+
+// The output must start with a hashtag (#).
+// Each word in the output must have its first letter capitalized.
+// If the final result, a single string, is longer than 140 characters, the function should return false.
+// If either the input (str) or the result is an empty string, the function should return false.
+// Examples
+// generateHashtag("    Hello     World   " ) ➞ "#HelloWorld"
+
+// generateHashtag("") ➞ false, "Expected an empty string to return false"
+
+// generateHashtag("Edabit Is Great") ➞ "#EdabitIsGreat", "Should remove spaces."
+// Notes
+// N/A
+
+// w.i.p
+
+function generateHashtag(str) {
+
+	if (!str || str.length >= 140 || /\s{2,}/.test(str)) return false;
+	
+	let splitString = str.split(' '), final = ``;
+	for(let i = 0; i < splitString.length; i++){
+			splitString[i] = splitString[i][0].toUpperCase() + splitString[i].substr(1);
+		final = splitString.join(' ');
+		}
+	final = `#` + final;
+	return final.replace(/\s+/g, '')
+	
+	
+	
+}
