1e6007581e6c2d067e10009684616742416a84aa
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2024-05-30 14:20:40 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -6793,3 +6793,25 @@ function doesBrickFit(a,b,c, w,h) {
 	
 	return (crossArea <= holeArea || lengthArea <= holeArea);
 }
+
+// White Spaces Between Lower and Uppercase Letters
+// Write a function that inserts a white space between every instance of a lower character followed immediately by an upper character.
+
+// Examples
+// insertWhitespace("SheWalksToTheBeach") ➞ "She Walks To The Beach"
+
+// insertWhitespace("MarvinTalksTooMuch") ➞ "Marvin Talks Too Much"
+
+// insertWhitespace("TheGreatestUpsetInHistory") ➞ "The Greatest Upset In History"
+// Notes
+// Each word in the phrase will be at least two characters long.
+
+function insertWhitespace(s) {
+	let arr = s.split('');
+	for(let i = 0; i < arr.length; i++){
+		if (arr[i] == arr[i].toUpperCase()){
+			arr[i] = " " + arr[i];
+		}
+	}
+	return arr.join('').trimStart();
+}
