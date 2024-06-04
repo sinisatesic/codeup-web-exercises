6ea1fa4a2150c3aa63c5cedd4d423902602da2dd
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2024-06-04 09:51:57 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -6815,3 +6815,55 @@ function insertWhitespace(s) {
 	}
 	return arr.join('').trimStart();
 }
+
+// Finish the Sentence You're On!
+// POV: You are in an exam and time has just run out. While the teacher's back is turned, you hastily take the opportunity to finish scribbling down the last few words of the conclusion.
+
+// For this challenge, it takes 0.5 seconds to write a character (not including spaces). Given the full sentence and the unfinished sentence as inputs, return the time it takes to finish writing in seconds.
+
+// Worked Example
+// timeToFinish(
+//   "And so brings my conclusion to its conclusion.",
+//   "And so brings my conclusion to"
+// ) ➞ 7
+
+// // "its" has 3 characters
+// // "conclusion." has 11 characters, including punctuation.
+// // 11 + 3 = 14
+// // 14 x 0.5 = 7
+// // Remember not to include spaces.
+// Examples
+// timeToFinish(
+//   "And so brings my conclusion to its conclusion.",
+//   "And so brings my conclusion to its conclus"
+// ) ➞ 2
+
+// timeToFinish(
+//   "As a result, my point is still valid.",
+//   "As a result, my"
+// ) ➞ 9
+
+// timeToFinish(
+//   "Thank you for reading my essay.",
+//   "T"
+// ) ➞ 12.5
+// Notes
+// The unfinished sentence is always found at the start of a complete sentence.
+
+function timeToFinish(full, part) {
+	let trimmedFull = full.replace(/ /g,''), trimmedPart = part.replace(/ /g, ''), counterOne = 0, counterTwo = 0;
+	for(let i = 0; i < trimmedFull.length; i++){
+		counterOne++;
+	}
+	for(let j = 0; j < trimmedPart.length; j++){
+			counterTwo++;
+		}
+	let a = counterOne - counterTwo;
+	return a*0.5;
+}
+
+// mozda naj bolji:
+
+function timeToFinish(full, part) {
+	return full.replace(part, '').replace(/\s/g, '').length * .5
+}
