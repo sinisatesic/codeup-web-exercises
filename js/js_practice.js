e307f70a8c91abd802981f49f5025872f6991d7e
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2024-06-18 10:13:21 -0500

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -6906,3 +6906,34 @@ function factory(num){
 		return ans;
 	}
 }
+
+// Swap the Numbers
+// Imagine you have three numbers: a, b, and c. c is equal to either a or b, but you don't know which one. Your task is to create a function that returns whatever number c isn't, out of a and b. So, if c is equal to a, return b, and if c is equal to b, return a. Here's what makes this challenge difficult: you cannot use any if statements.
+
+// Examples
+// swap(1, 0, 0) ➞ 1
+// // a = 1, b = 0, c = b
+// // return a
+
+// swap(1, 3, 1) ➞ 3
+// // a = 1, b = 3, c = a
+// // return b
+
+// swap(27, 31, 31) ➞ 27
+// // a = 27, b = 31, c = b
+// // return a
+// Notes
+// To prevent cheating, you also can't call any functions.
+// c will always be equal to either a or b.
+// a will never equal b.
+// a, b, and c will always be integers.
+
+function swap(a, b, c) {
+	return c === a ? b : c === b ? a : b;
+}
+
+// neznam kako:
+
+function swap(a, b, c) {
+	return a+b-c;
+}
