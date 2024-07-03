45caeb8da069c80fc0bef0f52675e607689bcca7
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2024-07-03 11:04:27 -0500

    promise stuff

--- js/dnd.js
+++ js/dnd.js
@@ -1,27 +1,44 @@
-console.log('test')
 
-let api = `https://www.dnd5eapi.co/api/`
 
+let api = `https://www.dnd5eapi.co/api/`;
+// let thumbnails = document.querySelector('.thumbnail')
+// let thumbnailImg = document.querySelector('.thumbnailImg');
+
+// const getMonsters = async () => {
+//     const monsterResponse = await fetch(api + `monsters`);
+//     const monsterResponseJSON = await monsterResponse.json();
+
+//     console.log(monsterResponseJSON);
+// }
 const getMonsters = async () => {
-    const monsterResponse = await fetch(api + `monsters`)
-    const monsterResponseJSON = await monsterResponse.json();
-
-    console.log(monsterResponseJSON);
+    fetch(api + `monsters`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log("TEST TEST");
+    })
+    .catch(error => console.error(error));
 }
 getMonsters();
 
 const getSpells = async () => {
-    const spellResponse = await fetch(api + `spells`)
+    const spellResponse = await fetch(api + `spells`);
     const spellResponseJSON = await spellResponse.json();
 
-    console.log(spellResponseJSON)
+    console.log(spellResponseJSON);
 }
 getSpells();
 
 const getFeatures = async () => {
-    const featureResponse = await fetch(api + `features`)
+    const featureResponse = await fetch(api + `features`);
     const featureResponseJSON = await featureResponse.json();
 
     console.log(featureResponseJSON);
 }
 getFeatures();
+
+//event listeners
+// thumbnails.addEventListener('mouseover', () => {
+//     thumbnailImg.classList.add('animate-bounce');
+//     console.log('bounce');
+// })
