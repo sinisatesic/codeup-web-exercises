66071bac85e9659221076597663bb78a4a298869
sinisatesic <56378424+sinisatesic@users.noreply.github.com>
2024-07-29 16:52:41 -0500

    api shenanigans

--- js/dnd.js
+++ js/dnd.js
@@ -1,6 +1,8 @@
 
 
 let api = `https://www.dnd5eapi.co/api/`;
+
+let test = document.getElementById("test");
 // let thumbnails = document.querySelector('.thumbnail')
 // let thumbnailImg = document.querySelector('.thumbnailImg');
 
@@ -10,18 +12,6 @@ let api = `https://www.dnd5eapi.co/api/`;
 
 //     console.log(monsterResponseJSON);
 // }
-const getMonsters = async () => {
-    try {
-        const monsterResponse = await fetch(api + `monsters`);
-        const monsterResponseJSON = await monsterResponse.json();
-        console.log("TESTY TEST TESTTSSTST")
-        console.log(monsterResponseJSON)
-    } catch(err) {
-        console.error(err)
-        console.error("ERROR MATE")
-    }
-}
-getMonsters();
 
 // const getMonsters = () => {
 //     fetch(api + `monsters`)
@@ -34,24 +24,73 @@ getMonsters();
 // }
 // getMonsters();
 
-const getSpells = async () => {
-    const spellResponse = await fetch(api + `spells`);
-    const spellResponseJSON = await spellResponse.json();
+// const getSpells = async () => {
+//     const spellResponse = await fetch(api + `spells`);
+//     const spellResponseJSON = await spellResponse.json();
 
-    console.log(spellResponseJSON);
-}
-getSpells();
+//     console.log(spellResponseJSON);
+// }
+// getSpells();
 
-const getFeatures = async () => {
-    const featureResponse = await fetch(api + `features`);
-    const featureResponseJSON = await featureResponse.json();
+// const getFeatures = async () => {
+//     const featureResponse = await fetch(api + `features`);
+//     const featureResponseJSON = await featureResponse.json();
 
-    console.log(featureResponseJSON);
-}
-getFeatures();
+//     console.log(featureResponseJSON);
+// }
+// getFeatures();
 
 //event listeners
 // thumbnails.addEventListener('mouseover', () => {
 //     thumbnailImg.classList.add('animate-bounce');
 //     console.log('bounce');
 // })
+const getMonsters = async () => {
+    try {
+        const monsterResponse = await fetch(api + `monsters`);
+        const monsterResponseJSON = await monsterResponse.json();
+        console.log("TESTY TEST TESTTSSTST")
+        console.log(monsterResponse)
+        console.log(monsterResponseJSON.count);
+        console.log(monsterResponseJSON.name)
+
+        let dynamicP = document.createElement("p");
+        dynamicP.classList.add("mt-5", "line-clamp-3", "text-sm", "leading-6", "text-gray-600");
+
+        monsterResponseJSON.results.forEach((e) => {
+            if (e == monsterResponseJSON.results[0]){
+                dynamicP.innerHTML = `${e.name}`
+                test.appendChild(dynamicP)
+            }
+        })
+    } catch(err) {
+        console.error(err)
+        console.error("ERROR MATE")
+    }
+}
+getMonsters();
+
+// const getSpells = async () => {
+//     try {
+//         const spellResponse = await fetch(api + `monsters`);
+//         const spellResponseJSON = await spellResponse.json();
+//         console.log(spellResponseJSON)
+//         console.log(spellResponseJSON.count);
+//     } catch(err){
+//         console.error(err);
+//     }
+// }
+// getSpells();
+
+// const getFeatures = async () => {
+//     try {
+//         const featureResponse = await fetch(api + `features`);
+//         const featureResponseJSON = await featureResponse.json();
+//         console.log(featureResponseJSON);
+//     } catch(err){
+//         console.error(err);
+//     }
+// }
+// getFeatures();
+
+
