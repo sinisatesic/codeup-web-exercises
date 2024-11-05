5c6d61c23525531d8ed920ba82d56ad6d2b8c76b
sinisatesic <sinisa.tesic210@gmail.com>
2024-11-05 13:24:51 -0600

    js practice

--- js/js_practice.js
+++ js/js_practice.js
@@ -7028,3 +7028,32 @@ function unrepeated(str) {
 	  }
 	  return arr.join("");
   }
+
+//   Inclusive Array Ranges
+// Write a function that, given the start startNum and end endNum values, return an array containing all the numbers inclusive to that range. See examples below.
+
+// Examples
+// inclusiveArray(1, 5) ➞ [1, 2, 3, 4, 5]
+
+// inclusiveArray(2, 8) ➞ [2, 3, 4, 5, 6, 7, 8]
+
+// inclusiveArray(10, 20) ➞ [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+// inclusiveArray(17, 5) ➞ [17]
+// Notes
+// The numbers in the array are sorted in ascending order.
+// If startNum is greater than endNum, return an array with the higher value. See example #4.
+// A recursive version of this challenge can be found via this link.
+
+// moj odgovor:
+
+function inclusiveArray(startNum, endNum) {
+	let ans = [];
+	if (startNum > endNum){
+		return [startNum];
+	}
+	for(let i = startNum; i <= endNum; i++){
+		ans.push(i)
+	}
+	return ans;
+}
