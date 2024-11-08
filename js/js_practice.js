2917fc8a42c35b0863630265a4370a93c2012270
sinisatesic <sinisa.tesic210@gmail.com>
2024-11-08 15:13:50 -0600

    another js prob

--- js/js_practice.js
+++ js/js_practice.js
@@ -7111,3 +7111,46 @@ const sumFoundIndexes = (arr, n) =>
 function rectangleInCircle(w, h, radius) {
 	return Math.sqrt(w**2 + h**2) >= radius*2 ? false : true
 }
+
+// Sum of all Evens in a Matrix
+// Create a function that returns the sum of all even elements in a 2D matrix.
+
+// Examples
+// sumOfEvens([
+//   [1, 0, 2],
+//   [5, 5, 7],
+//   [9, 4, 3]
+// ]) ➞ 6
+
+// // 2 + 4 = 6
+
+// sumOfEvens([
+//   [1, 1],
+//   [1, 1]
+// ]) ➞ 0
+
+// sumOfEvens([
+//   [42, 9],
+//   [16, 8]
+// ]) ➞ 66
+
+// sumOfEvens([
+//   [],
+//   [],
+//   []
+// ]) ➞ 0
+// Notes
+// Submatrices will be of equal length.
+// Return 0 if the 2D matrix only consists of empty submatrices.
+
+function sumOfEvens(arr) {
+	let ans = [];
+	for(let i = 0; i < arr.length; i++){
+		for(let j = 0; j < arr[i].length; j++){
+			if (arr[i][j] % 2 == 0){
+				ans.push(arr[i][j]);
+			}
+		}
+	}
+	return ans.reduce((a, b) => a + b, 0);
+}
