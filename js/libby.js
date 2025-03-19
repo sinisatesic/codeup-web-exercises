d75c0529a8f503c1f6b46153251f40d17c1714f8
sinisatesic <sinisa.tesic210@gmail.com>
2025-03-19 01:31:24 -0500

    libbysite ; css, js animations

--- js/libby.js
+++ js/libby.js
@@ -1,6 +1,11 @@
-console.log("test")
+// console.log("test")
 
-const navbar = document.querySelector(".navbar")
+const navbar = document.querySelector(".navbar");
+const cards = document.querySelectorAll(".card");
+const cardCenter = document.querySelector(".cardCenter");
+const cardBody = document.querySelector(".card-body");
+const cardTitle = document.querySelector(".card-title");
+const cardText = document.querySelector(".card-text");
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -11,4 +16,19 @@ document.addEventListener("DOMContentLoaded", () => {
             navbar.classList.remove("navbarScrolled");
         }
     })
-})
+});
+
+
+cards.forEach(card => {
+    const cardImage = card.querySelector(".cardImage");
+
+    card.addEventListener("mouseenter", () => {
+        cardImage.classList.remove("rounded-start");
+        cardImage.classList.add("cardImageCircleAnimation");
+    });
+
+    card.addEventListener("mouseleave", () => {
+        cardImage.classList.add("rounded-start");
+        cardImage.classList.remove("cardImageCircleAnimation");
+    });
+});
