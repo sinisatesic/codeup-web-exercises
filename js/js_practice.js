894670105fd035233e9adf38a2c9ab6638c871da
sinisatesic <sinisa.tesic210@gmail.com>
2025-08-01 01:02:17 -0500

    FIRST HARD LEETCODE YES

--- js/js_practice.js
+++ js/js_practice.js
@@ -7419,3 +7419,65 @@ let twoSum = (nums, target) => {
         }
     }
 }
+
+// 4. Median of Two Sorted Arrays
+// Solved
+// Hard
+// Topics
+// premium lock icon
+// Companies
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+// The overall run time complexity should be O(log (m+n)).
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [1,3], nums2 = [2]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
+// Example 2:
+
+// Input: nums1 = [1,2], nums2 = [3,4]
+// Output: 2.50000
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+
+// Constraints:
+
+// nums1.length == m
+// nums2.length == n
+// 0 <= m <= 1000
+// 0 <= n <= 1000
+// 1 <= m + n <= 2000
+// -106 <= nums1[i], nums2[i] <= 106
+
+var findMedianSortedArrays = function(nums1, nums2) {
+    let merged = nums1.concat(nums2).sort((a, b) => a - b, 0);
+    let mid = Math.floor(merged.length/2);
+    let halfFirst = merged.length/2 - 1
+    let halfSecond = merged.length/2
+    if (merged.length === 1){
+        return merged[0];
+    }
+    if ((nums1.length === 0 || nums2.length === 0) && merged.length % 2 === 0){
+        return (merged[mid - 1] + merged[mid])/2
+    }
+    if ((nums1.length === 0 || nums2.length === 0) && merged.length % 2 === 1){
+        return Math.ceil(merged[mid])
+    }
+    if(nums2.length == 0){
+        return Number(nums1);
+    }
+    if (merged.length <= 0){
+        return merged/2;
+    }
+    if (merged.length % 2 === 0){
+        return (merged[mid - 1] + merged[mid])/2
+    }
+    if (merged.length % 2 === 1){
+        return merged[mid];
+    }
+    return (merged[halfFirst] + merged[halfSecond])/2;
+};
